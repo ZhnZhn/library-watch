fbf15454cf3956494b1cf953d46798ab5d5a1c9f
Yevhenii Molodyi <eugmolody@gmail.com>
2020-11-05 14:10:57 +0200

    Edit custom plugins

--- plugins/post-processing.js
+++ plugins/post-processing.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 
+const LIB_PLACEHOLDER = '<!--script src="app/lib"></script-->'
+, _crLibTag = lib => '<script src="app/'+ lib + '" defer></script>';
+
 const postProcessing = prevHtml => {
   let nextHtml;
   try {
-    const str = fs.readFileSync('./dll/stats.json');
-    const stats = JSON.parse(str);
-    const lib = stats.lib;
-    nextHtml = prevHtml.replace(
-      '<!--lib-->',
-      '<script src="app/'+ lib + '" defer></script>'
-    );
+    const str = fs.readFileSync('./dll/stats.json')
+    , stats = JSON.parse(str)
+    , lib = stats.lib;    
+    nextHtml = prevHtml.replace(LIB_PLACEHOLDER, _crLibTag(lib));
   } catch(err) {
     console.log(err.message)
     return prevHtml;
