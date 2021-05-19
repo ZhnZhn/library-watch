d59b4f04e2573c1dfb0090e100222c3f6ea1306b
Yevhenii Molodyi <eugmolody@gmail.com>
2021-05-19 12:08:41 +0300

    Replace ToolbarButtonCircle by function component

--- js/components/dialogs/widgets/ToolbarButtonCircle.js
+++ js/components/dialogs/widgets/ToolbarButtonCircle.js
@@ -1,15 +1,11 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = _interopRequireWildcard(require("react"));
+var _react = require("react");
 
 var _Row = _interopRequireDefault(require("../rows/Row"));
 
@@ -19,68 +15,30 @@ var _jsxRuntime = require("react/jsx-runtime");
 
 var S = {
   ROW: {
-    paddingTop: '4px',
-    paddingBottom: '8px'
+    paddingTop: 4,
+    paddingBottom: 8
   },
   BUTTON_CIRCLE: {
-    marginLeft: '20px'
+    marginLeft: 20
   }
 };
-
-var ToolbarButtonCircle = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(ToolbarButtonCircle, _Component);
-
-  function ToolbarButtonCircle() {
-    var _this;
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-
-    _this._renderButtons = function (buttons) {
-      if (buttons === void 0) {
-        buttons = [];
-      }
-
-      return buttons.map(function (button, index) {
-        var caption = button.caption,
-            title = button.title,
-            onClick = button.onClick;
-        return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ButtonCircle["default"], {
-          caption: caption,
-          title: title,
-          style: S.BUTTON_CIRCLE,
-          onClick: onClick
-        }, caption + index);
-      });
-    };
-
-    return _this;
-  }
-
-  var _proto = ToolbarButtonCircle.prototype;
-
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.buttons === this.props.buttons) {
-      return false;
-    }
-
-    return true;
-  };
-
-  _proto.render = function render() {
-    var buttons = this.props.buttons;
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_Row["default"].Plain, {
-      style: S.ROW,
-      children: this._renderButtons(buttons)
-    });
-  };
-
-  return ToolbarButtonCircle;
-}(_react.Component);
-
+var ToolbarButtonCircle = /*#__PURE__*/(0, _react.memo)(function (_ref) {
+  var buttons = _ref.buttons;
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_Row["default"].Plain, {
+    style: S.ROW,
+    children: (buttons || []).map(function (_ref2, index) {
+      var caption = _ref2.caption,
+          title = _ref2.title,
+          onClick = _ref2.onClick;
+      return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ButtonCircle["default"], {
+        caption: caption,
+        title: title,
+        style: S.BUTTON_CIRCLE,
+        onClick: onClick
+      }, caption + index);
+    })
+  });
+});
 var _default = ToolbarButtonCircle;
 exports["default"] = _default;
 //# sourceMappingURL=ToolbarButtonCircle.js.map
--- src/components/dialogs/widgets/ToolbarButtonCircle.jsx
+++ src/components/dialogs/widgets/ToolbarButtonCircle.jsx
@@ -1,52 +1,30 @@
-import React, { Component } from 'react';
+import { memo } from 'react';
 
 import Row from '../rows/Row';
 import ButtonCircle from '../../zhn-atoms/ButtonCircle';
 
 const S = {
   ROW : {
-    paddingTop: '4px',
-    paddingBottom: '8px'
+    paddingTop: 4,
+    paddingBottom: 8
   },
   BUTTON_CIRCLE : {
-    marginLeft: '20px'
+    marginLeft: 20
   }
 };
 
-
-class ToolbarButtonCircle extends Component {
-
-  shouldComponentUpdate(nextProps, nextState){
-    if (nextProps.buttons === this.props.buttons){
-      return false;
-    }
-    return true;
-  }
-
-  _renderButtons = (buttons=[]) => {
-    return buttons.map((button, index) => {
-      const { caption, title, onClick } = button;
-      return (
-        <ButtonCircle
-          key={caption + index}
-          caption={caption}
-          title={title}
-          style={S.BUTTON_CIRCLE}
-          onClick={onClick}
-        />
-      );
-    })
-  }
-
-  render(){
-    const { buttons } = this.props;
-    return (
-      <Row.Plain style={S.ROW}>
-        {this._renderButtons(buttons)}
-      </Row.Plain>
-    );
-  }
-
-}
+const ToolbarButtonCircle = memo(({ buttons }) => (
+  <Row.Plain style={S.ROW}>
+    {(buttons || []).map(({ caption, title, onClick }, index) => (
+      <ButtonCircle
+        key={caption + index}
+        caption={caption}
+        title={title}
+        style={S.BUTTON_CIRCLE}
+        onClick={onClick}
+      />
+    ))}
+  </Row.Plain>
+));
 
 export default ToolbarButtonCircle
