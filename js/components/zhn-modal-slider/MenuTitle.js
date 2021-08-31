8cba8a46a04ec1d0280d08324a03e8b3d5898629
Yevhenii Molodyi <eugmolody@gmail.com>
2021-08-31 17:18:19 +0300

    Replace MenuTitle by function component

--- js/components/zhn-modal-slider/MenuTitle.js
+++ js/components/zhn-modal-slider/MenuTitle.js
@@ -1,15 +1,9 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
 exports["default"] = void 0;
-
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = _interopRequireWildcard(require("react"));
 
 var _MenuAriaItem = _interopRequireDefault(require("./MenuAriaItem"));
 
@@ -22,60 +16,48 @@ var S = {
   PREV_PAGE: {
     position: 'absolute',
     top: 0,
-    left: '16px'
+    left: 16
   },
   TITLE: {
-    paddingLeft: '16px'
+    paddingLeft: 16
   }
 };
 
-var MenuTitle = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(MenuTitle, _Component);
+var MenuTitle = function MenuTitle(_ref) {
+  var baseTitleCl = _ref.baseTitleCl,
+      title = _ref.title,
+      pageNumber = _ref.pageNumber,
+      onPrevPage = _ref.onPrevPage,
+      onReg = _ref.onReg;
 
-  function MenuTitle() {
-    return _Component.apply(this, arguments) || this;
+  if (!title) {
+    return null;
   }
 
-  var _proto = MenuTitle.prototype;
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_MenuAriaItem["default"], {
+    className: baseTitleCl,
+    style: S.ITEM,
+    onClick: onPrevPage.bind(null, pageNumber),
+    onReg: onReg,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+      style: S.PREV_PAGE,
+      children: '<'
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+      style: S.TITLE,
+      children: title
+    })]
+  });
+};
+/*
+MenuTitle.propTypes = {
+  baseTitleCl: PropTypes.string,
+  title: PropTypes.string,
+  pageNumber: PropTypes.number,
+  onPrevPage: PropTypes.func,
+  onReg: PropTypes.func
+}
+*/
 
-  /*
-  static propTypes = {
-    baseTitleCl: PropTypes.string,
-    title: PropTypes.string,
-    pageNumber: PropTypes.number,
-    onPrevPage: PropTypes.func,
-    onReg: PropTypes.func
-  }
-  */
-  _proto.render = function render() {
-    var _this$props = this.props,
-        baseTitleCl = _this$props.baseTitleCl,
-        title = _this$props.title,
-        pageNumber = _this$props.pageNumber,
-        onPrevPage = _this$props.onPrevPage,
-        onReg = _this$props.onReg;
-
-    if (!title) {
-      return null;
-    }
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_MenuAriaItem["default"], {
-      className: baseTitleCl,
-      style: S.ITEM,
-      onClick: onPrevPage.bind(null, pageNumber),
-      onReg: onReg,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-        style: S.PREV_PAGE,
-        children: '<'
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-        style: S.TITLE,
-        children: title
-      })]
-    });
-  };
-
-  return MenuTitle;
-}(_react.Component);
 
 var _default = MenuTitle;
 exports["default"] = _default;
--- src/components/zhn-modal-slider/MenuTitle.jsx
+++ src/components/zhn-modal-slider/MenuTitle.jsx
@@ -1,6 +1,4 @@
-import React, { Component } from 'react'
-
-import MenuAriaItem from './MenuAriaItem'
+import MenuAriaItem from './MenuAriaItem';
 
 const S = {
   ITEM: {
@@ -9,47 +7,46 @@ const S = {
   PREV_PAGE: {
     position: 'absolute',
     top: 0,
-    left: '16px'
+    left: 16
   },
   TITLE: {
-    paddingLeft: '16px'
+    paddingLeft: 16
   }
 };
 
-class MenuTitle extends Component {
-  /*
-  static propTypes = {
-    baseTitleCl: PropTypes.string,
-    title: PropTypes.string,
-    pageNumber: PropTypes.number,
-    onPrevPage: PropTypes.func,
-    onReg: PropTypes.func
-  }
-  */
-
-  render(){
-    const {
-      baseTitleCl,
-      title, pageNumber,
-      onPrevPage, onReg
-    } = this.props;
-    if (!title) { return null; }
-    return (
-      <MenuAriaItem
-        className={baseTitleCl}
-        style={S.ITEM}
-        onClick={onPrevPage.bind(null, pageNumber)}
-        onReg={onReg}
-      >
-        <span style={S.PREV_PAGE}>
-          {'<'}
-        </span>
-        <span style={S.TITLE}>
-          {title}
-        </span>
-      </MenuAriaItem>
-    );
-  }
+const MenuTitle = ({
+  baseTitleCl,
+  title,
+  pageNumber,
+  onPrevPage,
+  onReg
+}) => {
+  if (!title) { return null; }
+  return (
+    <MenuAriaItem
+      className={baseTitleCl}
+      style={S.ITEM}
+      onClick={onPrevPage.bind(null, pageNumber)}
+      onReg={onReg}
+    >
+      <span style={S.PREV_PAGE}>
+        {'<'}
+      </span>
+      <span style={S.TITLE}>
+        {title}
+      </span>
+    </MenuAriaItem>
+  );
 }
+
+/*
+MenuTitle.propTypes = {
+  baseTitleCl: PropTypes.string,
+  title: PropTypes.string,
+  pageNumber: PropTypes.number,
+  onPrevPage: PropTypes.func,
+  onReg: PropTypes.func
+}
+*/
 
 export default MenuTitle
