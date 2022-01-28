457b3690cbadbbef6c5d018411d4debeb83f6415
Yevhenii Molodyi <eugmolody@gmail.com>
2022-01-28 15:40:12 +0200

    Edit FormattedInteger function component

--- js/components/zhn-atoms/FormattedInteger.js
+++ js/components/zhn-atoms/FormattedInteger.js
@@ -1,41 +1,25 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _react = _interopRequireDefault(require("react"));
-
 var _jsxRuntime = require("react/jsx-runtime");
 
-//import PropTypes from "prop-types";
-var REPLACER_PATTERN = /(.)(?=(\d{3})+$)/g;
+var REPLACER_PATTERN = /(.)(?=(\d{3})+$)/g,
+    _formatValue = function _formatValue(value) {
+  return value >= 1000 ? ('' + value).replace(REPLACER_PATTERN, '$1,') : value;
+};
 
 var FormattedInteger = function FormattedInteger(_ref) {
-  var value = _ref.value,
-      style = _ref.style;
-
-  if (value >= 1000) {
-    value = ('' + value).replace(REPLACER_PATTERN, '$1,');
-  }
-
+  var style = _ref.style,
+      _ref$value = _ref.value,
+      value = _ref$value === void 0 ? 0 : _ref$value;
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
     style: style,
-    children: value
+    children: _formatValue(value)
   });
 };
-/*
-FormattedInteger.propTypes = {
-  value: PropTypes.number,
-  style: PropTypes.object
-}
-*/
 
-
-FormattedInteger.defaultProps = {
-  value: 0
-};
 var _default = FormattedInteger;
 exports["default"] = _default;
 //# sourceMappingURL=FormattedInteger.js.map
--- src/components/zhn-atoms/FormattedInteger.jsx
+++ src/components/zhn-atoms/FormattedInteger.jsx
@@ -1,29 +1,16 @@
-import React from 'react';
 
-//import PropTypes from "prop-types";
+const REPLACER_PATTERN = /(.)(?=(\d{3})+$)/g
+, _formatValue = value => value >= 1000
+   ? ('' + value).replace(REPLACER_PATTERN, '$1,')
+   : value;
 
-const REPLACER_PATTERN = /(.)(?=(\d{3})+$)/g;
-
-const FormattedInteger = ({ value, style }) => {
-  if (value >= 1000) {
-    value = ('' + value).replace(REPLACER_PATTERN, '$1,');
-  }
-
-  return (
-    <span style={style}>
-      {value}
-    </span>
-  );
-}
-
-/*
-FormattedInteger.propTypes = {
-  value: PropTypes.number,
-  style: PropTypes.object
-}
-*/
-FormattedInteger.defaultProps = {
-  value: 0
-}
+const FormattedInteger = ({
+  style,
+  value=0
+}) => (
+  <span style={style}>
+    {_formatValue(value)}
+  </span>
+);
 
 export default FormattedInteger
