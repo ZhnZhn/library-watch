842478a482ea4f565698414602e5066cc48967de
Yevhenii Molodyi <eugmolody@gmail.com>
2022-01-31 16:05:52 +0200

    Replace AlertDialog by function component

--- js/components/dialogs/AlertDialog.js
+++ js/components/dialogs/AlertDialog.js
@@ -1,15 +1,11 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = _interopRequireWildcard(require("react"));
+var _memoIsShow = _interopRequireDefault(require("./memoIsShow"));
 
 var _ModalDialog = _interopRequireDefault(require("../zhn-moleculs/ModalDialog"));
 
@@ -17,28 +13,26 @@ var _DialogStyles = _interopRequireDefault(require("../styles/DialogStyles"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var S = {
-  CAPTION: {
-    display: 'inline-block',
-    width: '400px',
-    paddingLeft: '10px',
-    color: '#F44336',
-    fontSize: '18px',
-    fontWeight: 'bold',
-    lineHeight: 2
-  },
-  ITEM_ID: {
-    color: 'rgba(164, 135, 212,1)',
-    fontWeight: 'bold'
-  },
-  DESCR: {
-    color: 'gray',
-    width: '400px',
-    paddingLeft: '10px',
-    fontWeight: 'bold',
-    lineHeight: 1.4,
-    whiteSpace: 'pre-line'
-  }
+var S_CAPTION = {
+  color: '#f44336',
+  display: 'inline-block',
+  width: 400,
+  paddingLeft: 10,
+  lineHeight: 2,
+  fontSize: '18px',
+  fontWeight: 'bold'
+},
+    S_ITEM_ID = {
+  color: 'rgba(164, 135, 212, 1)',
+  fontWeight: 'bold'
+},
+    S_DESCR = {
+  color: 'gray',
+  width: 400,
+  paddingLeft: 10,
+  lineHeight: 1.4,
+  whiteSpace: 'pre-line',
+  fontWeight: 'bold'
 };
 var ELLIPSIS = '...';
 
@@ -46,85 +40,66 @@ var _crItemId = function _crItemId(str) {
   return str ? str.substring(0, 20) + ELLIPSIS : '';
 };
 
-var AlertDialog = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(AlertDialog, _Component);
+var FN_NOOP = function FN_NOOP() {};
 
-  function AlertDialog() {
-    return _Component.apply(this, arguments) || this;
-  }
+var AlertDialog = function AlertDialog(_ref) {
+  var isShow = _ref.isShow,
+      data = _ref.data,
+      _ref$onClose = _ref.onClose,
+      onClose = _ref$onClose === void 0 ? FN_NOOP : _ref$onClose;
 
-  var _proto = AlertDialog.prototype;
+  var _ref2 = data || {},
+      alertCaption = _ref2.alertCaption,
+      alertItemId = _ref2.alertItemId,
+      alertDescr = _ref2.alertDescr,
+      caption = _ref2.caption,
+      itemId = _ref2.itemId,
+      descr = _ref2.descr,
+      _caption = alertCaption || caption || '',
+      _itemId = _crItemId(alertItemId || itemId),
+      _descr = alertDescr || descr || '';
 
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    data: PropTypes.shape({
-      alertCaption: PropTypes.string,
-      alertItemId: PropTypes.string,
-      alertDescr: PropTypes.string
-    }),
-    onClose: PropTypes.func
-  }
-  */
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps !== this.props && nextProps.isShow === this.props.isShow) {
-      return false;
-    }
-
-    return true;
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        isShow = _this$props.isShow,
-        data = _this$props.data,
-        onClose = _this$props.onClose,
-        _ref = data || {},
-        alertCaption = _ref.alertCaption,
-        alertItemId = _ref.alertItemId,
-        alertDescr = _ref.alertDescr,
-        caption = _ref.caption,
-        itemId = _ref.itemId,
-        descr = _ref.descr,
-        _caption = alertCaption || caption || '',
-        _itemId = _crItemId(alertItemId || itemId),
-        _descr = alertDescr || descr || '';
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_ModalDialog["default"], {
-      caption: "Alert",
-      isShow: isShow,
-      onClose: onClose,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-        style: _DialogStyles["default"].rowDiv,
-        children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
-          style: S.CAPTION,
-          children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-            children: _caption
-          }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-            children: ":"
-          }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-            style: S.ITEM_ID,
-            title: _itemId,
-            children: _itemId
-          })]
-        })
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-        style: _DialogStyles["default"].rowDiv,
-        children: /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
-          style: S.DESCR,
-          children: _descr
-        })
-      })]
-    });
-  };
-
-  return AlertDialog;
-}(_react.Component);
-
-AlertDialog.defaultProps = {
-  data: {},
-  onClose: function onClose() {}
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_ModalDialog["default"], {
+    caption: "Alert",
+    isShow: isShow,
+    onClose: onClose,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      style: _DialogStyles["default"].rowDiv,
+      children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
+        style: S_CAPTION,
+        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+          children: _caption
+        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+          children: ":"
+        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+          style: S_ITEM_ID,
+          title: _itemId,
+          children: _itemId
+        })]
+      })
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      style: _DialogStyles["default"].rowDiv,
+      children: /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+        style: S_DESCR,
+        children: _descr
+      })
+    })]
+  });
 };
-var _default = AlertDialog;
+/*
+AlertDialog.propTypes = {
+  isShow: PropTypes.bool,
+  data: PropTypes.shape({
+    alertCaption: PropTypes.string,
+    alertItemId: PropTypes.string,
+    alertDescr: PropTypes.string
+  }),
+  onClose: PropTypes.func
+}
+*/
+
+
+var _default = (0, _memoIsShow["default"])(AlertDialog);
+
 exports["default"] = _default;
 //# sourceMappingURL=AlertDialog.js.map
--- src/components/dialogs/AlertDialog.jsx
+++ src/components/dialogs/AlertDialog.jsx
@@ -1,93 +1,86 @@
-import React, { Component } from 'react'
-import ModalDialog from '../zhn-moleculs/ModalDialog';
-import styles from '../styles/DialogStyles'
+import memoIsShow from './memoIsShow';
 
-const S = {
-  CAPTION : {
-    display : 'inline-block',
-    width : '400px',
-    paddingLeft : '10px',
-    color : '#F44336',
-    fontSize: '18px',
-    fontWeight : 'bold',
-    lineHeight : 2
-  },
-  ITEM_ID : {
-    color: 'rgba(164, 135, 212,1)',
-    fontWeight : 'bold'
-  },
-  DESCR : {
-    color: 'gray',
-    width : '400px',
-    paddingLeft : '10px',
-    fontWeight: 'bold',
-    lineHeight : 1.4,
-    whiteSpace: 'pre-line'
-  }
+import ModalDialog from '../zhn-moleculs/ModalDialog';
+import STYLE from '../styles/DialogStyles';
+
+const S_CAPTION = {
+  color: '#f44336',
+  display: 'inline-block',
+  width: 400,
+  paddingLeft: 10,
+  lineHeight: 2,
+  fontSize: '18px',
+  fontWeight: 'bold',
+}
+, S_ITEM_ID = {
+  color: 'rgba(164, 135, 212, 1)',
+  fontWeight: 'bold'
+}
+, S_DESCR = {
+  color: 'gray',
+  width: 400,
+  paddingLeft: 10,
+  lineHeight: 1.4,
+  whiteSpace: 'pre-line',
+  fontWeight: 'bold'
 };
+
 
 const ELLIPSIS = '...';
 const _crItemId = str => str
   ? str.substring(0,20) + ELLIPSIS
   : '';
 
+const FN_NOOP = () => {};
 
-class AlertDialog extends Component {
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    data: PropTypes.shape({
-      alertCaption: PropTypes.string,
-      alertItemId: PropTypes.string,
-      alertDescr: PropTypes.string
-    }),
-    onClose: PropTypes.func
-  }
-  */
-  static defaultProps = {
-    data: {},
-    onClose: () => {}
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    if (nextProps !== this.props &&
-        nextProps.isShow === this.props.isShow) {
-      return false;
-    }
-    return true;
-  }
-
-  render(){
-    const { isShow, data, onClose } = this.props
-    , {
-      alertCaption, alertItemId, alertDescr,
-      caption, itemId, descr
-    } = data || {}
-    , _caption = alertCaption || caption || ''
-    , _itemId = _crItemId(alertItemId || itemId)
-    , _descr = alertDescr || descr || '';
-    return (
-      <ModalDialog
-        caption="Alert"
-        isShow={isShow}
-        onClose={onClose}
-      >
-         <div style={styles.rowDiv}>
-            <span style={S.CAPTION}>
-              <span>{_caption}</span>
-              <span>:</span>
-              <span style={S.ITEM_ID} title={_itemId}>
-                {_itemId}
-              </span>
+const AlertDialog = ({
+  isShow,
+  data,
+  onClose=FN_NOOP
+}) => {
+  const {
+    alertCaption,
+    alertItemId,
+    alertDescr,
+    caption,
+    itemId,
+    descr
+  } = data || {}
+  , _caption = alertCaption || caption || ''
+  , _itemId = _crItemId(alertItemId || itemId)
+  , _descr = alertDescr || descr || '';
+  return (
+    <ModalDialog
+      caption="Alert"
+      isShow={isShow}
+      onClose={onClose}
+    >
+       <div style={STYLE.rowDiv}>
+          <span style={S_CAPTION}>
+            <span>{_caption}</span>
+            <span>:</span>
+            <span style={S_ITEM_ID} title={_itemId}>
+              {_itemId}
             </span>
-         </div>
-         <div style={styles.rowDiv}>
-            <p style={S.DESCR}>{_descr}</p>
-         </div>
-      </ModalDialog>
-    );
-  }
+          </span>
+       </div>
+       <div style={STYLE.rowDiv}>
+          <p style={S_DESCR}>{_descr}</p>
+       </div>
+    </ModalDialog>
+  );
+};
+
+/*
+AlertDialog.propTypes = {
+  isShow: PropTypes.bool,
+  data: PropTypes.shape({
+    alertCaption: PropTypes.string,
+    alertItemId: PropTypes.string,
+    alertDescr: PropTypes.string
+  }),
+  onClose: PropTypes.func
 }
+*/
 
-
-export default AlertDialog
+export default memoIsShow(AlertDialog)
