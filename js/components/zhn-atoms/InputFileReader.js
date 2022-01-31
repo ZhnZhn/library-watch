324d21e1dd31b007353053ccef74977006f7813b
Yevhenii Molodyi <eugmolody@gmail.com>
2022-01-31 12:38:14 +0200

    Replace InputFileReader by function component

--- js/components/zhn-atoms/InputFileReader.js
+++ js/components/zhn-atoms/InputFileReader.js
@@ -1,134 +1,117 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = _interopRequireWildcard(require("react"));
+var _react = require("react");
 
 var _jsxRuntime = require("react/jsx-runtime");
 
 //import PropTypes from "prop-types";
-var InputFileReader = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(InputFileReader, _Component);
+var CL_FILE_INPUT = "_react-file-reader-input",
+    INITIAL_FILE_LABEL = "Choose a file...",
+    FN_NOOP = function FN_NOOP() {};
 
-  function InputFileReader() {
-    var _this;
+var InputFileReader = function InputFileReader(_ref) {
+  var _ref$as = _ref.as,
+      as = _ref$as === void 0 ? 'text' : _ref$as,
+      _ref$onChange = _ref.onChange,
+      onChange = _ref$onChange === void 0 ? FN_NOOP : _ref$onChange;
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
+  var _useState = (0, _react.useState)(INITIAL_FILE_LABEL),
+      labelText = _useState[0],
+      setLabelText = _useState[1],
+      _hChange = (0, _react.useCallback)(function (e) {
+    var files = [];
 
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-    _this.state = {
-      labelText: "Choose a file..."
-    };
-
-    _this._handleChange = function (e) {
-      var files = [];
-
-      for (var i = 0; i < e.target.files.length; i++) {
-        // Convert to Array.
-        files.push(e.target.files[i]);
-      } // Build Promise List, each promise resolved by FileReader.onload.
+    for (var i = 0; i < e.target.files.length; i++) {
+      // Convert to Array.
+      files.push(e.target.files[i]);
+    } // Build Promise List, each promise resolved by FileReader.onload.
 
 
-      Promise.all(files.map(function (file) {
-        return new Promise(function (resolve, reject) {
-          var reader = new FileReader();
+    // Build Promise List, each promise resolved by FileReader.onload.
+    Promise.all(files.map(function (file) {
+      return new Promise(function (resolve, reject) {
+        var reader = new FileReader();
 
-          reader.onload = function (result) {
-            // Resolve both the FileReader result and its original file.
-            resolve([result, file]);
-          }; // Read the file with format based on this.props.as.
+        reader.onload = function (result) {
+          // Resolve both the FileReader result and its original file.
+          resolve([result, file]);
+        }; // Read the file with format based on this.props.as.
 
 
-          switch (_this.props.as.toLowerCase()) {
-            case 'binary':
-              reader.readAsBinaryString(file);
-              break;
+        // Read the file with format based on this.props.as.
+        switch (as.toLowerCase()) {
+          case 'binary':
+            reader.readAsBinaryString(file);
+            break;
 
-            case 'buffer':
-              reader.readAsArrayBuffer(file);
-              break;
+          case 'buffer':
+            reader.readAsArrayBuffer(file);
+            break;
 
-            case 'text':
-              reader.readAsText(file);
-              break;
+          case 'text':
+            reader.readAsText(file);
+            break;
 
-            case 'url':
-              reader.readAsDataURL(file);
-              break;
+          case 'url':
+            reader.readAsDataURL(file);
+            break;
 
-            default:
-              reader.readAsText(file);
-          }
-        });
-      })).then(function (zippedResults) {
-        // Run the callback after all files have been read.
-        if (zippedResults && zippedResults[0]) {
-          var file = zippedResults[0][1];
-
-          _this.setState({
-            labelText: file.name
-          });
-
-          _this.props.onChange(zippedResults);
-        } else {
-          _this.setState({
-            labelText: 'Choose a file...'
-          });
-
-          _this.props.onChange(zippedResults);
+          default:
+            reader.readAsText(file);
         }
       });
-    };
+    })).then(function (zippedResults) {
+      // Run the callback after all files have been read.
+      var _labelText = zippedResults && zippedResults[0] ? (zippedResults[0][1] || {}).name : INITIAL_FILE_LABEL;
 
-    return _this;
-  }
-
-  var _proto = InputFileReader.prototype;
-
-  _proto.render = function render() {
-    var as = this.props.as,
-        labelText = this.state.labelText;
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      className: "_react-file-reader-input",
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("input", {
-        className: "inputfile",
-        type: "file",
-        id: "file",
-        name: "file",
-        as: as,
-        onChange: this._handleChange
-      }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("label", {
-        htmlFor: "file",
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("svg", {
-          xmlns: "http://www.w3.org/2000/svg",
-          width: "20",
-          height: "17",
-          viewBox: "0 0 20 17",
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-            d: "M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"
-          })
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-          children: labelText
-        })]
-      })]
+      setLabelText(_labelText);
+      onChange(zippedResults);
     });
-  };
+  }, []); // as, onChange
 
-  return InputFileReader;
-}(_react.Component);
+  /*eslint-enable react-hooks/exhaustive-deps */
 
-InputFileReader.defaultProps = {
-  as: 'text'
+
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    className: CL_FILE_INPUT,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("input", {
+      className: "inputfile",
+      type: "file",
+      id: "file",
+      name: "file",
+      as: as,
+      onChange: _hChange
+    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("label", {
+      htmlFor: "file",
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("svg", {
+        xmlns: "http://www.w3.org/2000/svg",
+        width: "20",
+        height: "17",
+        viewBox: "0 0 20 17",
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
+          d: "M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"
+        })
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+        children: labelText
+      })]
+    })]
+  });
 };
+/*
+ InputFileReader.propTypes = {
+   as: PropTypes.oneOf([
+    'binary',
+    'buffer',
+    'text',
+    'url'
+   ])
+ }
+*/
+
+
 var _default = InputFileReader;
 exports["default"] = _default;
 //# sourceMappingURL=InputFileReader.js.map
--- src/components/zhn-atoms/InputFileReader.jsx
+++ src/components/zhn-atoms/InputFileReader.jsx
@@ -1,90 +1,89 @@
-import React, { Component } from 'react';
-
 //import PropTypes from "prop-types";
+import { useState, useCallback } from 'react';
 
-class InputFileReader extends Component {
-  /*
-   static propTypes = {
-     as: PropTypes.oneOf(['binary', 'buffer', 'text', 'url'])
-   }
-   */
-   static defaultProps = {
-     as: 'text'
-   }
+const CL_FILE_INPUT = "_react-file-reader-input"
+, INITIAL_FILE_LABEL = "Choose a file..."
+, FN_NOOP = () => {};
 
-   state = {
-     labelText: "Choose a file..."
-   }
-      
-  _handleChange = (e) => {
-    const files = [];
-    for (let i = 0; i < e.target.files.length; i++) {
-      // Convert to Array.
-      files.push(e.target.files[i]);
-    }
-
-    // Build Promise List, each promise resolved by FileReader.onload.
-    Promise.all(files.map(file => new Promise((resolve, reject) => {
-      let reader = new FileReader();
-
-      reader.onload = result => {
-        // Resolve both the FileReader result and its original file.
-        resolve([result, file]);
-      };
-
-      // Read the file with format based on this.props.as.
-      switch ((this.props.as).toLowerCase()) {
-        case 'binary':
-          reader.readAsBinaryString(file);
-          break;
-        case 'buffer':
-          reader.readAsArrayBuffer(file);
-          break;
-        case 'text':
-          reader.readAsText(file);
-          break;
-        case 'url':
-          reader.readAsDataURL(file);
-          break;
-        default:
-          reader.readAsText(file);
-      }
-    })))
-    .then(zippedResults => {
-
-      // Run the callback after all files have been read.
-      if (zippedResults && zippedResults[0] ){
-        const file = zippedResults[0][1]
-        this.setState({ labelText: file.name })
-        this.props.onChange(zippedResults);
-      } else {
-        this.setState({ labelText: 'Choose a file...'});
-        this.props.onChange(zippedResults);
+const InputFileReader = ({
+  as='text',
+  onChange=FN_NOOP
+}) => {
+  const [labelText, setLabelText] = useState(INITIAL_FILE_LABEL)
+  /*eslint-disable react-hooks/exhaustive-deps */
+  , _hChange = useCallback((e) => {
+      const files = [];
+      for (let i = 0; i < e.target.files.length; i++) {
+        // Convert to Array.
+        files.push(e.target.files[i]);
       }
 
-    });
-  }
+      // Build Promise List, each promise resolved by FileReader.onload.
+      Promise.all(files.map(file => new Promise((resolve, reject) => {
+        let reader = new FileReader();
 
-  render() {
-    const { as } = this.props
-        , { labelText } = this.state
-    return (
-      <div className="_react-file-reader-input">
-        <input
-          className="inputfile"
-          type="file"
-          id="file"
-          name="file"
-          as={as}
-          onChange={this._handleChange}
-        />
-        <label htmlFor="file">
-           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg>
-           <span>{labelText}</span>
-        </label>
-       </div>
-    );
-  }
-}
+        reader.onload = result => {
+          // Resolve both the FileReader result and its original file.
+          resolve([result, file]);
+        };
+
+        // Read the file with format based on this.props.as.
+        switch (as.toLowerCase()) {
+          case 'binary':
+            reader.readAsBinaryString(file);
+            break;
+          case 'buffer':
+            reader.readAsArrayBuffer(file);
+            break;
+          case 'text':
+            reader.readAsText(file);
+            break;
+          case 'url':
+            reader.readAsDataURL(file);
+            break;
+          default:
+            reader.readAsText(file);
+        }
+      })))
+      .then(zippedResults => {
+        // Run the callback after all files have been read.
+        const _labelText = zippedResults && zippedResults[0]
+          ? (zippedResults[0][1] || {}).name
+          : INITIAL_FILE_LABEL;
+        setLabelText(_labelText)
+        onChange(zippedResults)
+      });
+    }, [])
+    // as, onChange
+    /*eslint-enable react-hooks/exhaustive-deps */
+
+  return (
+    <div className={CL_FILE_INPUT}>
+      <input
+        className="inputfile"
+        type="file"
+        id="file"
+        name="file"
+        as={as}
+        onChange={_hChange}
+      />
+      <label htmlFor="file">
+         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg>
+         <span>{labelText}</span>
+      </label>
+     </div>
+  );
+};
+
+/*
+ InputFileReader.propTypes = {
+   as: PropTypes.oneOf([
+    'binary',
+    'buffer',
+    'text',
+    'url'
+   ])
+ }
+*/
 
 export default InputFileReader
