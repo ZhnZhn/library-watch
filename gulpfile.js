afe741666d8fcef91d1254e0af2c9183b1ada2db
Yevhenii Molodyi <eugmolody@gmail.com>
2022-02-01 11:15:19 +0200

    Edit gulp's css minification task

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,41 @@
 'use strict';
 
-var gulp = require('gulp')
-  , del = require('del')
-  , concat = require('gulp-concat')
-  , cleanCss = require('gulp-clean-css')
-  , rev = require('gulp-rev')
-  , inject = require('gulp-inject')
-  , replace = require('gulp-replace')
-  , rename = require('gulp-rename');
+const gulp = require('gulp')
+, del = require('del')
+, concat = require('gulp-concat')
+, cleanCss = require('gulp-clean-css')
+, rev = require('gulp-rev')
+, replace = require('gulp-replace')
+, rename = require('gulp-rename')
+, fs = require('node:fs');
 
-gulp.task('clean', function(){
+gulp.task('clean', function cleanImpl(){
   return del(['./css/*.min.css']);
 })
 
-gulp.task('styles', gulp.series('clean', function(){
+gulp.task('styles', gulp.series('clean', function stylesImpl(){
   return gulp.src(['./css/*.css', '!./css/*.min.css'])
-    .pipe(concat('app.min.css'))
-    .pipe(cleanCss())
-    .pipe(rev())
-    .pipe(gulp.dest('./css'))
+   .pipe(concat('app.min.css'))
+   .pipe(cleanCss())
+   .pipe(rev())
+   .pipe(gulp.dest('./css'))
 }));
 
-gulp.task('inject-css', gulp.series('styles', function(){
-   var target = gulp.src('./template/gulp.ejs')
-     , source = gulp.src(['./css/*.min.css'], { read:false })
-   return target
-     .pipe(inject(source))
-     .pipe(gulp.dest('./template'))
-}));
+const _isMinCssFileName = name =>
+   name.indexOf('.min.css') !== -1;
+const _getCssMinFileName = () => fs
+   .readdirSync('./css')
+   .filter(_isMinCssFileName)[0];
+const _crCssMinLink = fileName => 
+   `<link rel="stylesheet" href="css/${fileName}">`;
+const _crCssLink = () => _crCssMinLink(_getCssMinFileName());
 
-gulp.task('template', gulp.series('inject-css', function(){
+
+gulp.task('template', gulp.series('styles', function templateImpl(){  
   return gulp.src('./template/gulp.ejs')
-    .pipe(replace('<!-- inject:css -->',''))
-    .pipe(replace('<!-- endinject  -->', ''))
+    .pipe(replace('<!-- REPLACE_BY_CSS_MIN_LINK -->', _crCssLink()))
     .pipe(rename('index.ejs'))
     .pipe(gulp.dest('./template'));
 }))
-
 
 gulp.task('default', gulp.series('template'));
