bfbec76eebeea0fc16286d1ce68cbbf4d06abdbe
Yevhenii Molodyi <eugmolody@gmail.com>
2022-02-01 12:28:58 +0200

    Replace ButtonSave by function component

--- js/components/zhn-moleculs/ButtonSave.js
+++ js/components/zhn-moleculs/ButtonSave.js
@@ -7,9 +7,9 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _react = require("react");
+
+var _useListen = _interopRequireDefault(require("../hooks/useListen"));
 
 var _WatchActions = _interopRequireWildcard(require("../../flux/actions/WatchActions"));
 
@@ -21,7 +21,6 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-//import PropTypes from "prop-types";
 var CAPTION = "S",
     TITLE = "Save Watch Items to Locale Storage",
     S_NOT_WATCH_EDITED = {
@@ -29,74 +28,33 @@ var CAPTION = "S",
   color: 'gray'
 };
 
-var ButtonSave = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(ButtonSave, _Component);
+var ButtonSave = function ButtonSave(_ref) {
+  var className = _ref.className,
+      style = _ref.style,
+      store = _ref.store;
 
-  /*
-  static propTypes = {
-    store : PropTypes.object.isRequired,
-    style : PropTypes.object
-  }
-  */
-  function ButtonSave(props) {
-    var _this;
+  var _useState = (0, _react.useState)(function () {
+    return store.getWatchEdited();
+  }),
+      isWatchEdited = _useState[0],
+      setIsWatchEdited = _useState[1];
 
-    _this = _Component.call(this) || this;
-
-    _this._onStore = function (actionType, value) {
-      if (actionType === _WatchActions.WatchActionTypes.SET_WATCH_EDITED) {
-        _this.setState({
-          isWatchEdited: value
-        });
-      }
-    };
-
-    _this.state = {
-      isWatchEdited: props.store.getWatchEdited()
-    };
-    return _this;
-  }
-
-  var _proto = ButtonSave.prototype;
-
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.isWatchEdited === this.state.isWatchEdited) {
-      return false;
+  (0, _useListen["default"])(store, function (actionType, value) {
+    if (actionType === _WatchActions.WatchActionTypes.SET_WATCH_EDITED) {
+      setIsWatchEdited(value);
     }
+  });
 
-    return true;
-  };
+  var _style = isWatchEdited ? style : (0, _extends2["default"])({}, style, S_NOT_WATCH_EDITED);
 
-  _proto.componentDidMount = function componentDidMount() {
-    var store = this.props.store;
-    this.unsubcribe = store.listen(this._onStore);
-    this.setState({
-      isWatchEdited: store.getWatchEdited()
-    });
-  };
-
-  _proto.componentWillUnmount = function componentWillUnmount() {
-    this.unsubcribe();
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        className = _this$props.className,
-        style = _this$props.style,
-        isWatchEdited = this.state.isWatchEdited,
-        _style = isWatchEdited ? style : (0, _extends2["default"])({}, style, S_NOT_WATCH_EDITED);
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ButtonCircle["default"], {
-      className: className,
-      caption: CAPTION,
-      title: TITLE,
-      style: _style,
-      onClick: _WatchActions["default"].saveWatch
-    });
-  };
-
-  return ButtonSave;
-}(_react.Component);
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ButtonCircle["default"], {
+    className: className,
+    caption: CAPTION,
+    title: TITLE,
+    style: _style,
+    onClick: _WatchActions["default"].saveWatch
+  });
+};
 
 var _default = ButtonSave;
 exports["default"] = _default;
--- src/components/zhn-moleculs/ButtonSave.jsx
+++ src/components/zhn-moleculs/ButtonSave.jsx
@@ -1,5 +1,6 @@
-import { Component } from 'react';
-//import PropTypes from "prop-types";
+import { useState } from 'react';
+
+import useListen from '../hooks/useListen';
 
 import WatchActions, { WatchActionTypes } from '../../flux/actions/WatchActions';
 import ButtonCircle from '../zhn-atoms/ButtonCircle';
@@ -11,59 +12,35 @@ const CAPTION = "S"
    color : 'gray'
 };
 
-class ButtonSave extends Component {
-  /*
-  static propTypes = {
-    store : PropTypes.object.isRequired,
-    style : PropTypes.object
-  }
-  */
+const ButtonSave = ({
+  className,
+  style,
+  store
+}) => {
+  const [
+    isWatchEdited,
+    setIsWatchEdited
+  ] = useState(() => store.getWatchEdited());
 
-  constructor(props){
-    super()
-    this.state = {
-      isWatchEdited : props.store.getWatchEdited()
-    }
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    if (nextState.isWatchEdited === this.state.isWatchEdited){
-      return false;
-    }
-    return true;
-  }
-
-  componentDidMount(){
-    const { store } = this.props
-    this.unsubcribe = store.listen(this._onStore)
-    this.setState({ isWatchEdited : store.getWatchEdited() })
-  }
-  _onStore = (actionType, value) => {
+  useListen(store, (actionType, value) => {
     if (actionType === WatchActionTypes.SET_WATCH_EDITED){
-      this.setState({ isWatchEdited : value })
+      setIsWatchEdited(value)
     }
-  }
-  componentWillUnmount(){
-    this.unsubcribe()
-  }
+  })
 
-  render(){
-    const { className, style } = this.props
-    , { isWatchEdited } = this.state
-    , _style = isWatchEdited
-        ? style
-        : {...style, ...S_NOT_WATCH_EDITED}
+  const _style = isWatchEdited
+    ? style
+    : {...style, ...S_NOT_WATCH_EDITED};
 
-    return (
-      <ButtonCircle
-         className={className}
-         caption={CAPTION}
-         title={TITLE}
-         style={_style}
-         onClick={WatchActions.saveWatch}
-      />
-    );
-  }
-}
+  return (
+    <ButtonCircle
+       className={className}
+       caption={CAPTION}
+       title={TITLE}
+       style={_style}
+       onClick={WatchActions.saveWatch}
+    />
+  );
+};
 
 export default ButtonSave
