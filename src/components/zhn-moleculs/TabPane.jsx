49384f4e2a63356eb5da62775cc05c6d44007d10
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-09 15:07:12 +0200

    Replace TabPane by function component

--- src/components/zhn-moleculs/TabPane.jsx
+++ src/components/zhn-moleculs/TabPane.jsx
@@ -1,5 +1,8 @@
-//import PropTypes from "prop-types";
-import React, { Component } from 'react';
+import {
+  useState,
+  cloneElement
+} from '../uiApi';
+import useRefInit from '../hooks/useRefInit';
 
 const S_TABS = {
   margin: '10px 5px 0 10px',
@@ -16,71 +19,49 @@ const S_TABS = {
 }
 , S_TABPANE_HIDED = { display: 'none' };
 
-
-class TabPane extends Component {
-  /*
-  static propTypes = {
-     width: PropTypes.number,
-     height: PropTypes.number,
-     children: PropTypes.arrayOf(PropTypes.node)
-  }
-  */
-
-  constructor(props){
-    super(props)
-    const components = props.children.map((tab, index) => {
-       return  React.cloneElement(tab.props.children, { key : 'comp' + index });
-    })
-    this.state = {
-      selectedTabIndex : 0,
-      components
-    }
-  }
-
-  _handlerClickTab = (index) => {
-    this.setState({selectedTabIndex : index});
-  }
-
-  _renderTabs = (children) => {
-      const {selectedTabIndex} = this.state;
-      return children.map((tab, index) => {
-        const isSelected = (index === selectedTabIndex) ? true : false;
-        return React.cloneElement(tab, { key : index, onClick : this._handlerClickTab.bind(null, index), isSelected })
-      })
-  }
-
-  _renderComponents = () => {
-     const {selectedTabIndex, components} = this.state;
-     return components.map((comp, index) => {
-        const divStyle = (index === selectedTabIndex)
-           ? S_TABPANE_SELECTED
-           : S_TABPANE_HIDED;
-         return (
-            <div style={divStyle} key={'a'+index}>
-               {comp}
-            </div>
-          )
-     })
-  }
-
-  render(){
-    const { children, width, height } = this.props;
-    return (
-      <div style={{ width, height }}>
-        <div style={S_TABS}>
-           {this._renderTabs(children)}
-        </div>
-        <div style={S_TABPANES}>
-           {this._renderComponents()}
-        </div>
+const TabPane = ({
+  width,
+  height,
+  children
+}) => {
+  const components = useRefInit(() => children
+    .map((tab, index) => cloneElement(
+        tab.props.children, { key: index }
+    )))
+  , [
+     selectedTabIndex,
+     setSelectedTabIndex
+  ] = useState(0)
+  , _isSelectedTabIndex = index =>
+      index === selectedTabIndex;
+  return (
+    <div style={{ width, height }}>
+      <div style={S_TABS}>
+         {children.map((tab, index) =>
+            cloneElement(tab, {
+              key: index,
+              onClick: () => setSelectedTabIndex(index),
+              isSelected: _isSelectedTabIndex(index)
+            })
+         )}
       </div>
-    )
-  }
-
-  getSelectedTabIndex(){
-    return this.state.selectedTabIndex;
-  }
-}
-
+      <div style={S_TABPANES}>
+         {components.map((comp, index) => {
+            const divStyle = _isSelectedTabIndex(index)
+               ? S_TABPANE_SELECTED
+               : S_TABPANE_HIDED;
+             return (
+               <div
+                 key={index}
+                 style={divStyle}
+                >
+                  {comp}
+               </div>
+             );
+         })}
+      </div>
+    </div>
+  );
+};
 
 export default TabPane
