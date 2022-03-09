cea59b033b37bb5f644c6319a018f595fb679e31
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-09 14:23:57 +0200

    Replace ModalPane by function component

---
+++ src/components/hooks/useClickOutside.js
@@ -0,0 +1,58 @@
+import {
+  useRef,
+  useCallback,
+  useEffect
+} from '../uiApi';
+
+const _getRefValue = ref => ref.current;
+
+const _removeClickListener = (
+  listener,
+  ref
+) => {
+  if (_getRefValue(ref)) {
+    document.removeEventListener('click', listener, true);
+    ref.current = null
+  }
+};
+
+const useClickOutside = (
+  isShow,
+  onClickOutside
+) => {
+  const _ref = useRef(null)
+  , _refIs = useRef(null)
+  /*eslint-disable react-hooks/exhaustive-deps */
+  , _hClickOutside = useCallback(event => {
+      const _el = _getRefValue(_ref);
+      if ( _el && _el.contains
+        && !_el.contains(event.target)
+      ){
+        event.stopPropagation()
+        onClickOutside(event)
+      }
+  }, []);
+  // onClickOutside
+  /*eslint-enable react-hooks/exhaustive-deps */
+
+  useEffect(() => {
+    if (isShow && !_getRefValue(_refIs)) {
+      document.addEventListener('click', _hClickOutside, true)
+      _refIs.current = true
+    } else if (!isShow) {
+      _removeClickListener(_hClickOutside, _refIs)
+    }
+  })
+
+  /*eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    return () => _removeClickListener(_hClickOutside, _refIs)
+  }, [])
+  // _hClickOutside
+  /*eslint-enable react-hooks/exhaustive-deps */
+
+  return _ref;
+};
+
+
+export default useClickOutside
--- src/components/zhn-moleculs/ModalPane.jsx
+++ src/components/zhn-moleculs/ModalPane.jsx
@@ -1,74 +1,22 @@
-import { Component } from 'react'
-//import PropTypes from 'prop-types'
+import useClickOutside from '../hooks/useClickOutside';
 
-const S_DIV = { backgroundColor: '#4d4d4d' };
+const S_DIV = { backgroundColor: '#4d4d4d' }
+, FN_NOOP = () => {};
 
-
-class ModalPane extends Component {
-  /*
-  static propTypes = {
-    className: PropTypes.string,
-    style: PropTypes.object,
-    theme: PropTypes.object,
-    isShow: PropTypes.bool,
-    onClose: PropTypes.func
-  }
-  */
-
-  static defaultProps = {
-    onClose: () => {}
-  }
-
-  _hClickOutside = (event) => {
-    if (this.rootNode
-      && this.rootNode.contains
-      && !this.rootNode.contains(event.target)
-    ){
-      event.stopPropagation()
-      this.props.onClose(event)
-    }
-  }
-
-  _addOutsideListener = () => {
-    document.addEventListener('click', this._hClickOutside, true)
-  }
-  _removeOutsideListener = () => {
-    document.removeEventListener('click', this._hClickOutside, true)
-  }
-
-  componentDidMount() {
-    if (this.props.isShow) {
-      this._addOutsideListener()
-    }
-  }
-  componentWillUnmount() {
-    this._removeOutsideListener()
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps ){
-      if (this.props.isShow){
-        this._addOutsideListener()
-      } else {
-        this._removeOutsideListener()
-      }
-    }
-  }
-
-  _refRootNode = n => this.rootNode = n
-
-  render(){
-    const { children } = this.props;
-
-    return (
-      <div
-         ref={this._refRootNode}
-         style={S_DIV}
-      >
-        {children}
-      </div>
-    );
-  }
-}
+const ModalPane = ({
+  isShow,
+  onClose=FN_NOOP,
+  children
+}) => {
+  const _ref = useClickOutside(isShow, onClose);
+  return (
+    <div
+       ref={_ref}
+       style={S_DIV}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default ModalPane
