5879c494402021bd7f79fc93ef89515fe10a6cf4
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-10 15:50:57 +0200

    Replace ModalDialogContainer by function component

--- src/components/zhn-containers/ModalDialogContainer.jsx
+++ src/components/zhn-containers/ModalDialogContainer.jsx
@@ -1,82 +1,27 @@
-import React, { Component } from 'react';
+const CL_INIT = 'modal-root'
+, CL_SHOWING = 'modal-root show-modal'
+, S_SHOW = { display: 'block' }
+, S_HIDE = { display: 'none' };
 
-//import PropTypes from "prop-types";
+const ModalDialogContainer = ({
+  isShow,
+  onClose,
+  children
+}) => {
+  const [_className, _style] = isShow
+    ? [CL_SHOWING, S_SHOW]
+    : [CL_INIT, S_HIDE];
 
-const CLASS = {
-  INIT : 'modal-root',
-  SHOWING : 'modal-root show-modal',
-  HIDING : 'modal-root hide-modal'
+  return (
+    <div
+      role="presentation"
+      className={_className}
+      style={_style}
+      onClick={onClose}
+    >
+      {children}
+    </div>
+  );
 };
-
-const STYLE = {
-  SHOW : {
-    display : 'block'
-  },
-  HIDE : {
-    display : 'none'
-  },
-  HIDE_BACKGROUND : {
-    backgroundColor : 'rgba(0,0,0, 0)'
-  }
-}
-
-class ModalDialogContainer extends Component {
-  /*
-  static propTypes = {
-    isShow  : PropTypes.bool,
-    timeout : PropTypes.number,
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node
-    ]),
-    onClose : PropTypes.func
-  }
-  */
-
-  static defaultProps = {
-     timeout: 450,
-     onClose: () => {}
-  }
-
-  constructor(props){
-    super()
-    this.wasClosing = true
-    this.state = {}
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    if (this.wasClosing){
-      setTimeout(() => {
-        this.setState({});
-      }, this.props.timeout)
-    }
-  }
-
-  render(){
-    const { isShow, children, onClose } = this.props;
-    let _className, _style;
-    if (this.wasClosing){
-       _className = CLASS.INIT;
-       _style = STYLE.HIDE;
-       this.wasClosing = false;
-    } else {
-      _className = isShow ? CLASS.SHOWING : CLASS.HIDING;
-      _style = isShow ? STYLE.SHOW : STYLE.HIDE_BACKGROUND;
-      if (!isShow){
-        this.wasClosing = true;
-      }
-    }
-
-    return (
-      <div
-        className={_className}
-        style={_style}
-        onClick={onClose}
-      >
-        {children}
-      </div>
-    )
-  }
-}
 
 export default ModalDialogContainer
