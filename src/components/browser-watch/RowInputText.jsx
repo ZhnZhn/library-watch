2e84538b543318a028dd605a6a5f7e71ca1cd820
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-18 14:13:04 +0200

    Replace RowInputText by function component

--- src/components/browser-watch/RowInputText.jsx
+++ src/components/browser-watch/RowInputText.jsx
@@ -1,54 +1,47 @@
-import React, { Component } from 'react';
+import {
+  forwardRef,
+  useRef,
+  useImperativeHandle
+} from '../uiApi';
 import InputText from '../zhn-atoms/InputText';
-import DialogStyles from '../styles/DialogStyles'
-import crRowCaption from './crRowCaption'
+import styles from '../styles/DialogStyles';
+import crRowCaption from './crRowCaption';
 
-const styles = DialogStyles;
-const S = {
-  ROOT: {
-    lineHeight: 2
-  },
-  CAPTION: {
-    width: 120
-  },
-  INPUT_TEXT: {
-    width : 250,
-    marginLeft : 0,
-    marginRight: 0,
-    paddingLeft: 10,
-    height: 30
-  }
-};
-
-
-class RowInputText extends Component {
-  /*
-  static propTypes = {
-    caption: PropTypes.string
-  },
-  */
-  _refInput = c => this.inputText = c
-
-  render(){    
-    return (
-      <div style={{ ...styles.rowDiv, ...S.ROOT }}>
-         <span style={{ ...styles.labelSpan, ...S.CAPTION }}>
-           {crRowCaption(this.props.caption)}
-         </span>
-         <InputText
-            ref={this._refInput}
-            style={S.INPUT_TEXT}
-         />
-      </div>
-    )
-  }
-
-  getValue(){
-    return this.inputText.getValue().trim();
-  }
-  setValue(value){
-    this.inputText.setValue(value);
-  }
+const S_ROW_DIV = { lineHeight: 2 }
+, S_CAPTION = { width: 120 }
+, S_INPUT_TEXT = {
+  width: 250,
+  marginLeft: 0,
+  marginRight: 0,
+  paddingLeft: 10,
+  height: 30
 }
+, _getRefValue = ref => ref.current;
+
+const RowInputText = forwardRef(({
+  caption
+}, ref) => {
+  const _refInput = useRef();
+
+  useImperativeHandle(ref, () => ({
+    getValue: () => _getRefValue(_refInput)
+      .getValue()
+      .trim(),
+    setValue: (value) => _getRefValue(_refInput)
+      .setValue(value)
+  }))
+
+  return (
+    <div style={{...styles.rowDiv, ...S_ROW_DIV}}>
+       <span style={{...styles.labelSpan, ...S_CAPTION}}>
+         {crRowCaption(caption)}
+       </span>
+       <InputText
+          ref={_refInput}
+          style={S_INPUT_TEXT}
+       />
+    </div>
+  );
+});
 
 export default RowInputText
