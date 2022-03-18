d3f718ffdbd4aa7712afa36189ce3544b776d61a
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-18 17:40:46 +0200

    Replace GroupAddPane by function component

--- src/components/browser-watch/GroupAddPane.jsx
+++ src/components/browser-watch/GroupAddPane.jsx
@@ -1,105 +1,105 @@
-import { Component } from 'react';
 //import PropTypes from 'prop-types'
+import {
+  useRef,
+  useState,
+  useCallback
+} from '../uiApi';
+import useListen from '../hooks/useListen';
 
 import RowInputText from './RowInputText';
 import FlatButton from '../zhn-m/FlatButton';
 import ValidationMessages from '../dialogs/rows/ValidationMessages';
 
-const S = {
-  COMMAND_DIV : {
-     cursor: 'default',
-     float: 'right',
-     marginTop: 8,
-     marginBottom: 10,
-     marginRight: 4
-  }
+const S_DIV_BTS = {
+  textAlign: 'right',
+  margin: '8px 4px 10px 0',
+  cursor: 'default'
+}
+, _getRefValue = ref => ref.current;
+
+const GroupAddPane = ({
+  store,
+  actionCompleted,
+  actionFailed,
+  forActionType,
+  msgOnIsEmptyName,
+  onCreate,
+  onClose
+}) => {
+  const _refInputText = useRef()
+  , [
+    validationMessages,
+    setValidationMesages
+  ] = useState([])
+  , _hClear = useCallback(() => {
+    _getRefValue(_refInputText).setValue("")
+    setValidationMesages([])
+  }, [])
+  /*eslint-disable react-hooks/exhaustive-deps */
+  , _hCreate = useCallback(() => {
+    const _inputComp = _getRefValue(_refInputText)
+    , caption = _inputComp.getValue();
+    if (caption){
+      onCreate({ caption });
+    } else {
+      _inputComp.setValue('')
+      setValidationMesages([msgOnIsEmptyName('Group')])
+    }
+  }, [])
+  // msgOnIsEmptyName, onCreate
+  /*eslint-enable react-hooks/exhaustive-deps */
+
+  useListen(store, (actionType, data) => {
+    if (data && data.forActionType === forActionType) {
+      if (actionType === actionCompleted) {
+        _hClear()
+      } else if (actionType === actionFailed) {
+        setValidationMesages(data.messages)
+      }
+    }
+  })
+
+  return (
+    <div>
+      <RowInputText
+         ref={_refInputText}
+         caption="Group"
+      />
+      <ValidationMessages
+         validationMessages={validationMessages}
+       />
+      <div style={S_DIV_BTS}>
+         <FlatButton
+            isPrimary={true}
+            caption="Create"
+            timeout={0}
+            onClick={_hCreate}
+         />
+         <FlatButton
+            caption="Clear"
+            timeout={0}
+            onClick={_hClear}
+         />
+         <FlatButton
+            caption="Close"
+            timeout={0}
+            onClick={onClose}
+         />
+       </div>
+    </div>
+  );
 };
 
-class GroupAddPane extends Component {
-  /*
-  static propTypes = {
-    store : PropTypes.object,
-    actionCompleted : PropTypes.string,
-    actionFailed : PropTypes.string,
-    forActionType : PropTypes.string,
-    msgOnIsEmptyName : PropTypes.func,
-    onCreate : PropTypes.func,
-    onClose : PropTypes.func
-  },
-  */
-  state = {
-    validationMessages: []
-  }
-
-
-  componentDidMount(){
-    this.unsubscribe = this.props
-      .store.listen(this._onStore);
-  }
-  componentWillUnmount(){
-    this.unsubscribe();
-  }
-  _onStore = (actionType, data) => {
-    const {actionCompleted, actionFailed, forActionType} = this.props;
-    if (actionType === actionCompleted && data.forActionType === forActionType){
-       this._handlerClear();
-    } else if (actionType === actionFailed && data.forActionType === forActionType){
-       this.setState({validationMessages: data.messages});
-    }
-  }
-
-  _handlerClear = () => {
-    this.inputText.setValue('');
-    if (this.state.validationMessages.length>0){
-       this.setState({validationMessages: []});
-    }
-  }
-
-  _handlerCreate = () => {
-     const caption = this.inputText.getValue();
-     if (caption){
-       this.props.onCreate({caption});
-     } else {
-       this.inputText.setValue('');
-       this.setState({validationMessages:[this.props.msgOnIsEmptyName('Group')]});
-     }
-  }
-
-  _refInputText = c => this.inputText = c
-
-  render(){
-    const {onClose} = this.props
-        , {validationMessages} = this.state;
-    return (
-      <div>
-        <RowInputText
-           ref={this._refInputText}
-           caption="Group"
-        />
-        <ValidationMessages
-           validationMessages={validationMessages}
-         />
-        <div style={S.COMMAND_DIV}>
-           <FlatButton
-              isPrimary={true}
-              caption="Create"
-              timeout={0}
-              onClick={this._handlerCreate}
-           />
-           <FlatButton
-              caption="Clear"
-              timeout={0}
-              onClick={this._handlerClear}
-           />
-           <FlatButton
-              caption="Close"
-              timeout={0}
-              onClick={onClose}
-           />
-         </div>
-      </div>
-    )
-  }
+/*
+GroupAddPane.propTypes = {
+  store: PropTypes.object,
+  actionCompleted: PropTypes.string,
+  actionFailed: PropTypes.string,
+  forActionType: PropTypes.string,
+  msgOnIsEmptyName: PropTypes.func,
+  onCreate: PropTypes.func,
+  onClose: PropTypes.func
 }
+*/
 
 export default GroupAddPane
