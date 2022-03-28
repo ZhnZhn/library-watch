5a6ca5a3f98fdd4ee938d0ef68853f53f118b427
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-28 17:04:53 +0300

    Replace WatchGroups by function component

--- src/components/browser-watch/WatchGroups.jsx
+++ src/components/browser-watch/WatchGroups.jsx
@@ -1,10 +1,10 @@
-import { Component } from 'react';
-
-import withDnDStyle from './decorators/withDnDStyle';
-import withDnDGroup from './decorators/withDnDGroup';
-
-import WatchActions from '../../flux/actions/WatchActions';
-import DRAG from './WatchDnDConfig';
+import {
+  hDragStartGroup,
+  hDragEnterGroup,
+  hDragOverGroup,
+  hDragLeaveGroup,
+  hDropGroup
+} from './dnd-handlers/DnDGroupHandlers';
 
 import Comp from '../Comp';
 import WatchLists from './WatchLists';
@@ -13,44 +13,30 @@ const { OpenClose2 } = Comp
 , _isArr = Array.isArray
 , S_GROUP_DIV = { lineHeight: 2 }
 
-@withDnDStyle
-@withDnDGroup
-class WatchGroups extends Component {
-
-  constructor(props){
-    super(props)
-    this._bindDnDGroup(DRAG, WatchActions)
-  }
-
-  render(){
-    const {
-      isModeEdit,
-      groups
-    } = this.props;
-
-    return _isArr(groups) ? groups
-      .map(({caption, lists}) => (
-          <OpenClose2
-             key={caption}
-             style={S_GROUP_DIV}
-             caption={caption}
-             isClose={true}
-             isDraggable={isModeEdit}
-             option={{ caption }}
-             onDragStart={this._hDragStartGroup}
-             onDragEnter={this._hDragEnterGroup}
-             onDragOver={this._hDragOverGroup}
-             onDragLeave={this._hDragLeaveGroup}
-             onDrop={this._hDropGroup}
-           >
-             <WatchLists
-               isModeEdit={isModeEdit}
-               groupCaption={caption}
-               lists={lists}
-             />
-           </OpenClose2>
-      )) : null;
-  }
-}
+const WatchGroups = ({
+  isModeEdit,
+  groups
+}) => _isArr(groups) ? groups
+  .map(({caption, lists}) => (
+      <OpenClose2
+         key={caption}
+         style={S_GROUP_DIV}
+         caption={caption}
+         isClose={true}
+         isDraggable={isModeEdit}
+         option={{ caption }}
+         onDragStart={hDragStartGroup}
+         onDragEnter={hDragEnterGroup}
+         onDragOver={hDragOverGroup}
+         onDragLeave={hDragLeaveGroup}
+         onDrop={hDropGroup}
+       >
+         <WatchLists
+           isModeEdit={isModeEdit}
+           groupCaption={caption}
+           lists={lists}
+         />
+       </OpenClose2>
+  )) : null;
 
 export default WatchGroups
