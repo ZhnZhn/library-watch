67eb4a0cacb290755ac149a8df884e60b74bb257
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-04 17:24:20 +0300

    Replace DialogType3 by function component

--- src/components/dialogs/DialogType3.jsx
+++ src/components/dialogs/DialogType3.jsx
@@ -1,17 +1,25 @@
-import { Component } from 'react'
-//import PropTypes from "prop-types";
+import {
+  useRef,
+  useCallback,
+  getRefValue,
+  setRefValue
+} from '../uiApi';
+import useToggle from '../hooks/useToggle';
+import useDialog from './useDialog';
+import useDialogButtons from './useDialogButtons';
+import memoIsShow from './memoIsShow';
 
-import D from './DialogCell'
-import Decor from './decorators/Decorators'
-import helperFns from './helperFns/helperFns'
+import Dialog from './Dialog';
+import D from './DialogCell';
+
+import helperFns from './helperFns/helperFns';
 
 const {
   dateConfig,
-  crMenuMore, crButtons,
   toUTCSecond
 } = helperFns;
 
-const _sortOptions = [
+const _SORT_OPTIONS = [
   { caption: "Activity, Recent Day", value: "activity" },
   { caption: "Creation Date", value: "creation"},
   { caption: "Score", value: "votes" },
@@ -24,161 +32,121 @@ const {
   _onTestDate
 } = dateConfig;
 
-@Decor.dialog
-class DialogType3 extends Component {
-  /*
-  static propTypes = {
-    caption: PropTypes.string,
-    requestType: PropTypes.string,
-    oneTitle: PropTypes.string,
-    onePlaceholder: PropTypes.string,
-    twoTitle: PropTypes.string,
-    twoPlaceholder: PropTypes.string,
-    isShow: PropTypes.bool,
-    onShow: PropTypes.func
+const _createValidationMessages = (
+  isValid,
+  datesMsg
+) => {
+  const msg = [];
+  if (!isValid) {
+    msg.push(datesMsg)
   }
-  */
-  constructor(props){
-    super(props)
-    this.stock = null
-    this.sortByItem = {}
-    this.toolbarButtons = this._createType2WithToolbar(props, {
-      hasDate: true
-    })
-    this._menuMore = crMenuMore(this, {
-      toggleDates: this._clickDateWithToolbar,
-      toggleLabels: this._clickLabelWithToolbar,
-      toggleToolBar: this._toggleWithToolbar,
-    })
-    this._commandButtons = crButtons({ inst: this })
-    this.state = {
-      ...this._withInitialState()
-    }
-  }
+  msg.isValid = (msg.length === 0);
+  return msg;
+};
 
-  shouldComponentUpdate(nextProps, nextState){
-    if (this.props !== nextProps){
-       if (this.props.isShow === nextProps.isShow){
-          return false;
-       }
-    }
-    return true;
-  }
-
- _handleSelectSortBy = (item) => {
-   this.sortByItem = item
- }
-
- _handleClear = () => {
-    this.inputOne.setValue('')
-    this.inputTwo.setValue('')
-    this.setState({ validationMessages: [] })
- }
-
- _handleLoad = () => {
-    this._handleLoadWithValidation(
-      this._createValidationMessages(),
-      this._createLoadOption
+const DialogType3 = memoIsShow(({
+  isShow,
+  caption,
+  oneTitle,
+  onePlaceholder,
+  twoTitle,
+  twoPlaceholder,
+  requestType,
+  onShow,
+  onLoad,
+  onClose
+}) => {
+  const [
+    isShowDate,
+    toggleIsShowDate
+  ] = useToggle()
+  , [
+    MENU_MODEL,
+    TOOLBAR_BUTTONS,
+    isToolbar,
+    isShowLabels
+  ] = useDialog(toggleIsShowDate)
+  , _refInputOne = useRef()
+  , _refInputTwo = useRef()
+  , _refInputDates = useRef()
+  , _refSortByItem = useRef({})
+  , _hSelectSortBy = useCallback(item => {
+    setRefValue(_refSortByItem, item)
+  }, [])
+  , [
+    validationMessages,
+    COMMAND_BUTTONS,
+    hClose,
+    hLoad
+  ] = useDialogButtons((
+    setValidationMessages,
+    clearValidationMessages
+  ) => {
+    const repo = getRefValue(_refInputOne).getValue()
+    , intitle = getRefValue(_refInputTwo).getValue()
+    , _dateInst = getRefValue(_refInputDates)
+    , { isValid, datesMsg } = _dateInst.getValidation()
+    , { fromDate, toDate } = _dateInst.getValues()
+    , _validationMessage = _createValidationMessages(
+      isValid, datesMsg
     )
-  }
+    if (_validationMessage.isValid) {
+      onLoad({
+        repo,
+        requestType,
+        intitle,
+        sort: (getRefValue(_refSortByItem) || {}).value,
+        fromdate: toUTCSecond(fromDate),
+        todate: toUTCSecond(toDate)
+      })
+    } else {
+      setValidationMessages(_validationMessage)
+    }
+  }, onClose);
 
-  _createValidationMessages = () => {
-      let msg = [];
-
-      const { isValid, datesMsg } = this.datesFragment.getValidation();
-      if (!isValid) { msg = msg.concat(datesMsg) }
-
-      msg.isValid = (msg.length === 0) ? true : false;
-      return msg;
-  }
- _createLoadOption = () => {
-   const repo = this.inputOne.getValue()
-       , intitle = this.inputTwo.getValue()
-       , { fromDate, toDate } = this.datesFragment.getValues()
-       , _fromDate = toUTCSecond(fromDate)
-       , _toDate = toUTCSecond(toDate)
-       , { requestType } = this.props
-       , { value } = this.sortByItem;
-
-   return {
-     repo, requestType, intitle,
-     sort : value,
-     fromdate : _fromDate,
-     todate : _toDate
-   };
- }
-
- _handleClose = () => {
-    this._handleCloseWithValidation(
-       this._createValidationMessages
-    )
-  }
-
-  _refInputOne = c => this.inputOne = c
-  _refInputTwo = c => this.inputTwo = c
-  _refDatesFragment = c => this.datesFragment = c
-
-  render(){
-    const {
-        caption, isShow, onShow,
-        oneTitle, onePlaceholder,
-        twoTitle, twoPlaceholder
-      } = this.props
-    , {
-      isToolbar,
-      isShowLabels,
-      isShowDate,
-      validationMessages
-    } = this.state;
-
-    return (
-       <D.DraggableDialog
-           isShow={isShow}
-           caption={caption}
-           menuModel={this._menuMore}
-           commandButtons={this._commandButtons}
-           onShowChart={onShow}
-           onClose={this._handleClose}
-       >
-         <D.Toolbar
-            isShow={isToolbar}
-            buttons={this.toolbarButtons}
-         />
-        <D.RowInputText
-           ref={this._refInputOne}
-           isShowLabel={isShowLabels}
-           caption={oneTitle}
-           placeholder={onePlaceholder}
-           onEnter={this._handleLoad}
+  return (
+    <Dialog
+       isShow={isShow}
+       isToolbar={isToolbar}
+       caption={caption}
+       menuModel={MENU_MODEL}
+       toolbarButtons={TOOLBAR_BUTTONS}
+       commandButtons={COMMAND_BUTTONS}
+       validationMessages={validationMessages}
+       onShow={onShow}
+       onClose={hClose}
+    >
+      <D.RowInputText
+         ref={_refInputOne}
+         isShowLabel={isShowLabels}
+         caption={oneTitle}
+         placeholder={onePlaceholder}
+         onEnter={hLoad}
+      />
+      <D.RowInputText
+         ref={_refInputTwo}
+         isShowLabel={isShowLabels}
+         caption={twoTitle}
+         placeholder={twoPlaceholder}
+      />
+      <D.RowInputSelect
+         isShowLabel={isShowLabels}
+         caption="Sort By"
+         placeholder="Default: Activity"
+         options={_SORT_OPTIONS}
+         onSelect={_hSelectSortBy}
+      />
+      <D.ShowHide isShow={isShowDate}>
+        <D.Dates
+           ref={_refInputDates}
+           isShowLabels={isShowLabels}
+           initFromDate={_initFromDate}
+           initToDate={_initToDate}
+           onTestDate={_onTestDate}
         />
-        <D.RowInputText
-           ref={this._refInputTwo}
-           isShowLabel={isShowLabels}
-           caption={twoTitle}
-           placeholder={twoPlaceholder}
-        />
-        <D.RowInputSelect
-           isShowLabel={isShowLabels}
-           caption="Sort By"
-           placeholder="Default: Activity"
-           options={_sortOptions}
-           onSelect={this._handleSelectSortBy}
-        />
-        <D.ShowHide isShow={isShowDate}>
-          <D.Dates
-              ref={this._refDatesFragment}
-              isShowLabels={isShowLabels}
-              initFromDate={_initFromDate}
-              initToDate={_initToDate}
-              onTestDate={_onTestDate}
-          />
-        </D.ShowHide>
-        <D.ValidationMessages
-           validationMessages={validationMessages}
-        />
-      </D.DraggableDialog>
-    );
-  }
-}
+      </D.ShowHide>
+    </Dialog>
+  );
+});
 
 export default DialogType3
