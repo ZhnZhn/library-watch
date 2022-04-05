8af28124748bf4de232ad8525a8c87edc747430e
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-05 17:12:03 +0300

    Edit import uiApi for ToolbarButtonCircle

--- src/components/dialogs/widgets/ToolbarButtonCircle.jsx
+++ src/components/dialogs/widgets/ToolbarButtonCircle.jsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo } from '../../uiApi';
 
 import Row from '../rows/Row';
 import ButtonCircle from '../../zhn-atoms/ButtonCircle';
@@ -6,15 +6,17 @@ import ButtonCircle from '../../zhn-atoms/ButtonCircle';
 const S_ROW = { padding: '4px 0 8px 0' }
 , S_BUTTON_CIRCLE = { marginLeft: 20 };
 
-const ToolbarButtonCircle = memo(({ buttons }) => (
+const ToolbarButtonCircle = memo(({
+  buttons
+}) => (
   <Row.Plain style={S_ROW}>
     {(buttons || []).map(
       ({caption, title, onClick}, index) => (
          <ButtonCircle
            key={caption + index}
+           style={S_BUTTON_CIRCLE}
            caption={caption}
            title={title}
-           style={S_BUTTON_CIRCLE}
            onClick={onClick}
          />
      ))}
