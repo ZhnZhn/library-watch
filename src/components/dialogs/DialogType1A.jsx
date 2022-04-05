e30a40dad034123078ebfb85cc3160ddc55472c6
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-05 16:37:06 +0300

    Replace DialogType1A by function component

--- src/components/dialogs/DialogType1A.jsx
+++ src/components/dialogs/DialogType1A.jsx
@@ -1,13 +1,11 @@
-import { Component } from 'react'
-//import PropTypes from "prop-types";
+import { getRefValue } from '../uiApi';
+import useDialog from './useDialog';
+import useSelectItem from './useSelectItem';
+import useDialogButtons from './useDialogButtons';
+import memoIsShow from './memoIsShow';
 
-import DateUtils from '../../utils/DateUtils'
-
-import D from './DialogCell'
-import Decor from './decorators/Decorators'
-import helperFns from './helperFns/helperFns'
-
-const { crMenuMore, crButtons } = helperFns;
+import Dialog from './Dialog';
+import D from './DialogCell';
 
 const MARKET_SHARES = [
   { caption: "OS: Desktop, Mobile, Tablet, Console", value: "os"},
@@ -16,8 +14,8 @@ const MARKET_SHARES = [
   { caption: "Android: Mobile, Tablet", value: "android-mobile" },
   { caption: "IOS: Mobile, Tablet", value: "ios-mobile" },
   { caption: "Browser: All Platforms", value: "browser" }
-];
-const REGIONS = [
+]
+, REGIONS = [
   { caption: "Worldwide", value: "worlwide", v2: "ww"  },
   { caption: "Africa", value: "africa", v2: "af" },
   { caption: "Asia", value: "asia", v2: "as" },
@@ -27,153 +25,84 @@ const REGIONS = [
   { caption: "North America", value: "north-america", v2: "na" },
   { caption: "South America", value: "south-america", v2: "sa" },
   { caption: "Oceania", value: "oceania", v2: "oc" }
-];
+]
+, _getItemCaption = item => item.caption
+, DF_MARKET_SHARE = MARKET_SHARES[0]
+, ITEM_PLACEHOLDER = _getItemCaption(DF_MARKET_SHARE)
+, DF_REGION = REGIONS[0]
+, REGION_PLACEHOLDER = _getItemCaption(DF_REGION);
 
-const _initFromDate = DateUtils.getFromDate(1)
-    , _initToDate = DateUtils.getToDate()
-    , _onTestDate = DateUtils.isValidDate;
-
-@Decor.dialog
-class DialogType3 extends Component {
-  /*
-  static propTypes = {
-    caption: PropTypes.string,
-    requestType: PropTypes.string,
-    oneTitle: PropTypes.string,
-    onePlaceholder: PropTypes.string,
-    twoTitle: PropTypes.string,
-    twoPlaceholder: PropTypes.string,
-    isShow: PropTypes.bool,
-    onShow: PropTypes.func
-  }
-  */
-  constructor(props){
-    super(props)
-    this.stock = null
-    this._item = MARKET_SHARES[0];
-    this._region = REGIONS[0];
-    this.toolbarButtons = this._createType2WithToolbar(props, {
-      hasDate: false
-    })
-    this._menuMore = crMenuMore(this, {
-      toggleDates: this._clickDateWithToolbar,
-      toggleLabels: this._clickLabelWithToolbar,
-      toggleToolBar: this._toggleWithToolbar,
-    })
-    this._commandButtons = crButtons({ inst: this })
-    this.state = {
-      ...this._withInitialState()
-    }
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    if (this.props !== nextProps){
-       if (this.props.isShow === nextProps.isShow){
-          return false;
-       }
-    }
-    return true;
-  }
-
- _hSelectItem = (item) => {
-   this._item = item
- }
- _hSelectRegion = item => {
-   this._region = item
- }
-
- _handleClear = () => {
-    this.setState({ validationMessages: [] })
- }
-
- _handleLoad = () => {
-    this._handleLoadWithValidation(
-      this._createValidationMessages(),
-      this._createLoadOption
-    )
-  }
-
-  _createValidationMessages = () => {
-      let msg = [];
-
-      const { isValid, datesMsg } = this.datesFragment.getValidation();
-      if (!isValid) { msg = msg.concat(datesMsg) }
-
-      msg.isValid = (msg.length === 0) ? true : false;
-      return msg;
-  }
- _createLoadOption = () => {
-   const { requestType } = this.props
-   , { value, caption } = this._item;
-   return {
-     requestType,
-     value,
-     caption: caption,
-     region: this._region
-   };
- }
-
- _handleClose = () => {
-    this._handleCloseWithValidation(
-       this._createValidationMessages
-    )
-  }
-
-  _refDatesFragment = c => this.datesFragment = c
-
-  render(){
+const DialogType3 = memoIsShow(({
+  isShow,
+  caption,
+  requestType,
+  onShow,
+  onLoad,
+  onClose
+}) => {
+  const [
+    MENU_MODEL,
+    TOOLBAR_BUTTONS,
+    isToolbar,
+    isShowLabels
+  ] = useDialog()
+  , [
+    _refItem,
+    _hSelectItem
+  ] = useSelectItem(DF_MARKET_SHARE)
+  , [
+    _refRegion,
+    _hSelectRegion
+  ] = useSelectItem(DF_REGION)
+  , [
+    validationMessages,
+    COMMAND_BUTTONS,
+    hClose
+  ] = useDialogButtons((
+    setValidationMessages,
+    clearValidationMessages
+  ) => {
     const {
-        caption, isShow, onShow,
-      } = this.props
-    , {
-      isToolbar,
-      isShowLabels,
-      isShowDate,
-      validationMessages
-    } = this.state;
+      value,
+      caption
+    } = getRefValue(_refItem ) || DF_MARKET_SHARE;
+    onLoad({
+      requestType,
+      value,
+      caption,
+      region: getRefValue(_refRegion) || DF_REGION
+    });
+  }, onClose);
 
-    return (
-       <D.DraggableDialog
-           isShow={isShow}
-           caption={caption}
-           menuModel={this._menuMore}
-           commandButtons={this._commandButtons}
-           onShowChart={onShow}
-           onClose={this._handleClose}
-       >
-         <D.Toolbar
-            isShow={isToolbar}
-            buttons={this.toolbarButtons}
-         />
-        <D.RowInputSelect
-           isShowLabel={isShowLabels}
-           caption="Item"
-           placeholder={MARKET_SHARES[0].caption}
-           options={MARKET_SHARES}
-           onSelect={this._hSelectItem}
-        />
-        <D.RowInputSelect
-           isShowLabel={isShowLabels}
-           caption="Region"
-           placeholder={REGIONS[0].caption}
-           options={REGIONS}
-           onSelect={this._hSelectRegion}
-        />
-        <D.ShowHide isShow={isShowDate}>
-          <D.Dates
-              ref={this._refDatesFragment}
-              isShowLabels={isShowLabels}
-              initFromDate={_initFromDate}
-              initToDate={_initToDate}
-              onTestDate={_onTestDate}
-          />
-        </D.ShowHide>
-        <D.ValidationMessages
-           validationMessages={validationMessages}
-        />
-      </D.DraggableDialog>
-    );
-  }
-}
+
+  return (
+    <Dialog
+       isShow={isShow}
+       isToolbar={isToolbar}
+       caption={caption}
+       menuModel={MENU_MODEL}
+       toolbarButtons={TOOLBAR_BUTTONS}
+       commandButtons={COMMAND_BUTTONS}
+       validationMessages={validationMessages}
+       onShow={onShow}
+       onClose={hClose}
+    >
+      <D.RowInputSelect
+         isShowLabel={isShowLabels}
+         caption="Item"
+         placeholder={ITEM_PLACEHOLDER}
+         options={MARKET_SHARES}
+         onSelect={_hSelectItem}
+      />
+      <D.RowInputSelect
+         isShowLabel={isShowLabels}
+         caption="Region"
+         placeholder={REGION_PLACEHOLDER}
+         options={REGIONS}
+         onSelect={_hSelectRegion}
+      />
+    </Dialog>
+  );
+});
 
 export default DialogType3
