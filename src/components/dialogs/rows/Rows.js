37005534139ba323f42a968d7774b7036b2d970c
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-06 16:27:23 +0300

    Edit Rows

--- src/components/dialogs/rows/Rows.js
+++ src/components/dialogs/rows/Rows.js
@@ -1,10 +1,14 @@
 
+import Caption from './Caption';
+import Row from './Row';
 import RowInputText from './RowInputText'
 import RowInputSelect from './RowInputSelect'
 import Dates from './Dates'
 import ValidationMessages from './ValidationMessages'
 
 const Rows = {
+  Caption,
+  Row,
   RowInputText,
   RowInputSelect,
   Dates,
