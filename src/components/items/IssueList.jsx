cc091689e754287dc34023c34a02d6f99aa1dc7c
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-12 13:10:35 +0300

    Edit IssueList style

--- src/components/items/IssueList.jsx
+++ src/components/items/IssueList.jsx
@@ -2,19 +2,17 @@ import A from '../zhn-atoms/A';
 import CL from '../styles/CL';
 import STYLE from './Item.Style';
 
-const S = {
-  STATE: {
-    ...STYLE.PR_8,
-    color: '#d7bb52'
-  },
-  NUMBER: {
-    ...STYLE.PR_8,
-    color: '#80c040'
-  },
-  DATE: {
-    ...STYLE.PR_8,
-    color: 'gray'
-  }
+const S_STATE = {
+  ...STYLE.PR_8,
+  color: '#d7bb52'
+}
+, S_NUMBER = {
+  ...STYLE.PR_8,
+  color: '#80c040'
+}
+, S_DATE = {
+  ...STYLE.PR_8,
+  color: 'gray'
 };
 
 const _toDate = strDate => (''+strDate)
@@ -23,7 +21,14 @@ const _toDate = strDate => (''+strDate)
 
 const IssueList = ({ issues }) => (issues || [])
   .map((item, index) => {
-    const { state, number, created_at, updated_at, title, html_url } = item
+    const {
+      state,
+      number,
+      created_at,
+      updated_at,
+      title,
+      html_url
+    } = item
     , _creadedAt = _toDate(created_at)
     , _updatedAt = created_at !== updated_at
         ? _toDate(updated_at)
@@ -32,16 +37,16 @@ const IssueList = ({ issues }) => (issues || [])
       <div key={index} className={CL.ROW_ITEM}>
         <A.Link href={html_url}>
            <div style={STYLE.PB_8}>
-             <span style={S.STATE}>
+             <span style={S_STATE}>
                {state}
              </span>
-             <span style={S.NUMBER}>
+             <span style={S_NUMBER}>
                {`(#${number})`}
              </span>
-             <span style={S.DATE}>
+             <span style={S_DATE}>
                {_creadedAt}
              </span>
-             <span style={S.DATE}>
+             <span style={S_DATE}>
                {_updatedAt}
              </span>
            </div>
