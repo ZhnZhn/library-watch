6d7ddf01019f3d7edea802780c31e8988f8e2712
Yevhenii Molodyi <eugmolody@gmail.com>
2023-08-07 15:56:03 +0300

    Add function factory _fEditItem to flux browserFn

--- src/flux/browserFn.js
+++ src/flux/browserFn.js
@@ -1,11 +1,12 @@
-import BrowserMenu from '../constants/BrowserMenu';
+import { BrowserType as BT } from '../constants/Type';
+
 import {
   setIsOpen,
   plusCounter,
   resetCounter
 } from './stores/browser/BrowserLogicFn';
 
-const _browserMenu = BrowserMenu;
+const _browserMenu = Object.create(null);
 
 export const setBrowserMenu = (
   browserType,
@@ -17,18 +18,20 @@ export const getBrowserMenu = (
   browserType
 ) => _browserMenu[browserType]
 
-export const setMenuItemOpen = (chartType, browserType) => {
-  setIsOpen(chartType, _browserMenu, browserType, true);
+const _isBrowserTypeWatchList = (
+  bT
+) => bT === BT.WATCH_LIST;
+const _fEditItem = (edit, value) => (
+  chartType,
+  browserType
+) => {
+  if(_isBrowserTypeWatchList(browserType)) {
+    edit(chartType, _browserMenu[browserType], value)
+  }
 }
-export const setMenuItemClose = (chartType, browserType) => {
-  setIsOpen(chartType, _browserMenu, browserType, false);
-}
-export const addMenuItemCounter = (chartType, browserType) => {
-  plusCounter(chartType, browserType, _browserMenu, 1);
-}
-export const minusMenuItemCounter = (chartType, browserType) => {
-  plusCounter(chartType, browserType, _browserMenu, -1);
-}
-export const resetMenuItemCounter = (cT, bT) => {
-  resetCounter(_browserMenu, bT, cT)
-}
+
+export const setMenuItemOpen = _fEditItem(setIsOpen, true)
+export const setMenuItemClose = _fEditItem(setIsOpen, false)
+export const addMenuItemCounter = _fEditItem(plusCounter, 1)
+export const minusMenuItemCounter = _fEditItem(plusCounter, -1)
+export const resetMenuItemCounter = _fEditItem(resetCounter)
