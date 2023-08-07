41cc82abb9a6d072c28a055a75b7be4ca479753d
Yevhenii Molodyi <eugmolody@gmail.com>
2023-08-07 15:54:50 +0300

    Add react reducer atom named export to flux storeApi

--- js/flux/storeApi.js
+++ js/flux/storeApi.js
@@ -2,12 +2,34 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports.fCrUse = exports.createStoreWithSelector = void 0;
+exports.fCrUse = exports.createStoreWithSelector = exports.atom = void 0;
 var _zustand = require("zustand");
 var _middleware = require("zustand/middleware");
+var _uiApi = require("../components/uiApi");
 var _useSubscribe = _interopRequireDefault(require("../components/hooks/useSubscribe"));
 const createStoreWithSelector = crStore => (0, _zustand.createStore)((0, _middleware.subscribeWithSelector)(crStore));
 exports.createStoreWithSelector = createStoreWithSelector;
 const fCrUse = (store, select) => _useSubscribe.default.bind(null, store, select);
 exports.fCrUse = fCrUse;
+const _isFn = v => typeof v === 'function',
+  _reducerUseAtomValue = (value, crOrValue) => _isFn(crOrValue) ? crOrValue(value) : crOrValue;
+const atom = initialValue => {
+  const _atom = Object.create(null);
+  _atom.value = initialValue;
+  return {
+    useAtomValue: () => {
+      const [value, dispatch] = (0, _uiApi.useReducer)(_reducerUseAtomValue, initialValue);
+      _atom.dispatch = dispatch;
+      return value;
+    },
+    setValue: crOrValue => {
+      _atom.value = _reducerUseAtomValue(_atom.value, crOrValue);
+      const _dispatch = _atom.dispatch;
+      if (_isFn(_dispatch)) {
+        _dispatch(crOrValue);
+      }
+    }
+  };
+};
+exports.atom = atom;
 //# sourceMappingURL=storeApi.js.map
--- src/flux/storeApi.js
+++ src/flux/storeApi.js
@@ -1,5 +1,7 @@
 import { createStore } from 'zustand';
 import { subscribeWithSelector } from 'zustand/middleware';
+
+import { useReducer } from '../components/uiApi';
 import useSubscribe from '../components/hooks/useSubscribe';
 
 export const createStoreWithSelector = (
@@ -12,3 +14,35 @@ export const fCrUse = (
   store,
   select
 ) => useSubscribe.bind(null, store, select);
+
+const _isFn = v => typeof v === 'function'
+, _reducerUseAtomValue = (
+  value,
+  crOrValue
+) => _isFn(crOrValue)
+  ? crOrValue(value)
+  : crOrValue;
+export const atom = (initialValue) => {
+   const _atom = Object.create(null);
+   _atom.value = initialValue
+   return {
+     useAtomValue: () => {
+       const [
+         value,
+         dispatch
+       ] = useReducer(
+         _reducerUseAtomValue,
+         initialValue
+       );
+       _atom.dispatch = dispatch
+       return value;
+     },
+     setValue: (crOrValue) => {
+       _atom.value = _reducerUseAtomValue(_atom.value, crOrValue)
+       const _dispatch = _atom.dispatch;
+       if (_isFn(_dispatch)) {
+         _dispatch(crOrValue)
+       }
+     }
+   };
+}
