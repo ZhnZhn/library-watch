b470befb90a1deefbcfdea886ea5187c24dbbc83
Yevhenii Molodyi <eugmolody@gmail.com>
2023-10-25 18:03:41 +0300

    Add styles CL to ShowHide function component

--- js/components/zhn-atoms/ShowHide.js
+++ js/components/zhn-atoms/ShowHide.js
@@ -1,40 +1,28 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
-exports["default"] = void 0;
-
-var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
-
+exports.default = void 0;
 var _crCn = _interopRequireDefault(require("../zhn-utils/crCn"));
-
+var _CL = require("../styles/CL");
 var _jsxRuntime = require("react/jsx-runtime");
-
-var CL_SHOW_POPUP = "show-popup",
-    S_SHOW = {
-  display: 'block'
-},
-    S_HIDE = {
-  display: 'none'
-};
-
-var ShowHide = function ShowHide(_ref) {
-  var isShow = _ref.isShow,
-      style = _ref.style,
-      className = _ref.className,
-      children = _ref.children;
-
-  var _className = (0, _crCn["default"])(className, [isShow, CL_SHOW_POPUP]),
-      _style = isShow ? S_SHOW : S_HIDE;
-
+const ShowHide = _ref => {
+  let {
+    isShow,
+    style,
+    className,
+    children
+  } = _ref;
+  const _className = (0, _crCn.default)(className, [isShow, _CL.CL_SHOW_POPUP]),
+    _style = isShow ? _CL.S_BLOCK : _CL.S_NONE;
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
     className: _className,
-    style: (0, _extends2["default"])({}, style, _style),
+    style: {
+      ...style,
+      ..._style
+    },
     children: children
   });
 };
-
-var _default = ShowHide;
-exports["default"] = _default;
+var _default = exports.default = ShowHide;
 //# sourceMappingURL=ShowHide.js.map
--- src/components/zhn-atoms/ShowHide.jsx
+++ src/components/zhn-atoms/ShowHide.jsx
@@ -1,8 +1,10 @@
 import crCn from '../zhn-utils/crCn';
+import {
+  S_BLOCK,
+  S_NONE,
+  CL_SHOW_POPUP
+} from '../styles/CL';
 
-const CL_SHOW_POPUP = "show-popup"
-, S_SHOW = { display: 'block' }
-, S_HIDE = { display: 'none' };
 
 const ShowHide = ({
   isShow,
@@ -13,7 +15,7 @@ const ShowHide = ({
   const _className = crCn(
      className, [isShow, CL_SHOW_POPUP]
   )
-  , _style = isShow ? S_SHOW : S_HIDE;
+  , _style = isShow ? S_BLOCK : S_NONE;
 
   return (
     <div
