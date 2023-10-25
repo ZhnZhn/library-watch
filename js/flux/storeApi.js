103d7d681255b6b212cee7758be7cb13395b1a6b
Yevhenii Molodyi <eugmolody@gmail.com>
2023-10-25 16:37:29 +0300

    Add function factory fCrStoreSlice named export to flux storeApi

--- js/flux/storeApi.js
+++ js/flux/storeApi.js
@@ -2,7 +2,7 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports.getStoreApi = exports.fCrUse = exports.createStoreWithSelector = exports.atom = void 0;
+exports.getStoreApi = exports.fCrUse = exports.fCrStoreSlice = exports.createStoreWithSelector = exports.atom = void 0;
 var _zustandLite = require("./zustand-lite");
 var _bindTo = require("../utils/bindTo");
 exports.bindTo = _bindTo.bindTo;
@@ -12,6 +12,12 @@ const createStoreWithSelector = crStore => (0, _zustandLite.createStore)((0, _zu
 exports.createStoreWithSelector = createStoreWithSelector;
 const getStoreApi = store => [store.setState, store.getState];
 exports.getStoreApi = getStoreApi;
+const fCrStoreSlice = (slicePn, optionPn) => [value => ({
+  [slicePn]: optionPn ? {
+    [optionPn]: value
+  } : value
+}), state => state[slicePn]];
+exports.fCrStoreSlice = fCrStoreSlice;
 const fCrUse = (store, select) => (0, _bindTo.bindTo)(_useSubscribe.default, store, select);
 exports.fCrUse = fCrUse;
 const _isFn = v => typeof v === 'function',
