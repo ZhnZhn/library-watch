9a572b0652a2cf29580d84bb5caf715393430c0c
Yevhenii Molodyi <eugmolody@gmail.com>
2023-10-27 11:43:43 +0300

    Add unit test suite storeApi.test

---
+++ js/components/hooks/useRerender.js
@@ -0,0 +1,9 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+var _uiApi = require("../uiApi");
+const _reducer = () => ({});
+const useRerender = () => (0, _uiApi.useReducer)(_reducer)[1];
+var _default = exports.default = useRerender;
+//# sourceMappingURL=useRerender.js.map
