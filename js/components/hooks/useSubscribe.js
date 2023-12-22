02b9e592cc40ced0f08eb6d8a2c664b347db8c9e
Yevhenii Molodyi <eugmolody@gmail.com>
2023-12-22 13:36:21 +0200

    Edit useSubscribe

--- js/components/hooks/useSubscribe.js
+++ js/components/hooks/useSubscribe.js
@@ -6,9 +6,7 @@ exports.default = void 0;
 var _uiApi = require("../uiApi");
 var _useRefInit = _interopRequireDefault(require("./useRefInit"));
 const useSubscribe = (store, selector, onChange) => {
-  const _subscribe = (0, _useRefInit.default)(() => rerender => (rerender(), store.subscribe(selector, onChange)))[0],
-    _getSnapshot = (0, _useRefInit.default)(() => () => selector(store.getState()))[0];
-  (0, _uiApi.useSyncExternalStore)(_subscribe, _getSnapshot);
+  (0, _uiApi.useSyncExternalStore)(...(0, _useRefInit.default)(() => [() => store.subscribe(selector, onChange), () => selector(store.getState())])[0]);
 };
 var _default = exports.default = useSubscribe;
 //# sourceMappingURL=useSubscribe.js.map
--- src/components/hooks/useSubscribe.js
+++ src/components/hooks/useSubscribe.js
@@ -6,12 +6,12 @@ const useSubscribe = (
   selector,
   onChange
 ) => {
-  const _subscribe = useRefInit(() => (rerender) => (rerender(), store.subscribe(selector, onChange)))[0]
-  , _getSnapshot = useRefInit(() => () => selector(store.getState()))[0];
   useSyncExternalStore(
-    _subscribe,
-    _getSnapshot
+    ...useRefInit(() => [
+      () => store.subscribe(selector, onChange),
+      () => selector(store.getState())
+    ])[0]
   )
-}
+};
 
 export default useSubscribe
