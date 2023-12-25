719a142228afd9c3af10fd3fdac8071aca0f5d2d
Yevhenii Molodyi <eugmolody@gmail.com>
2023-12-25 16:41:34 +0200

    Add function FN_NOOP to useKeyEscape

--- src/components/hooks/useKeyEscape.js
+++ src/components/hooks/useKeyEscape.js
@@ -1,11 +1,11 @@
+import { FN_NOOP } from '../uiApi'
 import { HAS_KEYBOARD_FOCUS } from '../has';
 import fUseKey from './fUseKey';
 
 const _isKeyEscape = evt => evt.keyCode === 27
  || evt.key === 'Escape'
-, FN_VOID = void 0
 , useKeyEscape = HAS_KEYBOARD_FOCUS
    ? fUseKey(_isKeyEscape)
-   : FN_VOID;
+   : FN_NOOP;
 
 export default useKeyEscape
