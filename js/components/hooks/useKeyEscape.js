ce002af6cc33093f67b129a28b39234212ad64c2
Yevhenii Molodyi <eugmolody@gmail.com>
2023-12-25 16:18:51 +0200

    Add HAS_KEYBOARD_FOCUS to useKeyEscape

--- js/components/hooks/useKeyEscape.js
+++ js/components/hooks/useKeyEscape.js
@@ -3,8 +3,10 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports.default = void 0;
+var _has = require("../has");
 var _fUseKey = _interopRequireDefault(require("./fUseKey"));
 const _isKeyEscape = evt => evt.keyCode === 27 || evt.key === 'Escape',
-  useKeyEscape = (0, _fUseKey.default)(_isKeyEscape);
+  FN_VOID = void 0,
+  useKeyEscape = _has.HAS_KEYBOARD_FOCUS ? (0, _fUseKey.default)(_isKeyEscape) : FN_VOID;
 var _default = exports.default = useKeyEscape;
 //# sourceMappingURL=useKeyEscape.js.map
--- src/components/hooks/useKeyEscape.js
+++ src/components/hooks/useKeyEscape.js
@@ -1,7 +1,11 @@
+import { HAS_KEYBOARD_FOCUS } from '../has';
 import fUseKey from './fUseKey';
 
-const _isKeyEscape = evt =>
-  evt.keyCode === 27 || evt.key === 'Escape'
-, useKeyEscape = fUseKey(_isKeyEscape);
+const _isKeyEscape = evt => evt.keyCode === 27
+ || evt.key === 'Escape'
+, FN_VOID = void 0
+, useKeyEscape = HAS_KEYBOARD_FOCUS
+   ? fUseKey(_isKeyEscape)
+   : FN_VOID;
 
 export default useKeyEscape
