36171679a8b6e239776b7b812896c7040125e22c
Yevhenii Molodyi <eugmolody@gmail.com>
2023-12-25 16:07:32 +0200

    Add HAS_KEYBOARD_FOCUS to useRecentFocusedElement

--- js/components/hooks/useRecentFocusedElement.js
+++ js/components/hooks/useRecentFocusedElement.js
@@ -3,11 +3,15 @@
 exports.__esModule = true;
 exports.default = void 0;
 var _uiApi = require("../uiApi");
+var _has = require("../has");
+const DF_TUPLE = [void 0, _uiApi.FN_NOOP];
 const useRecentFocusedElement = () => {
   const _refRecentFocusedElement = (0, _uiApi.useRef)();
-  return (0, _uiApi.useMemo)(() => [evt => {
+  /*eslint-disable react-hooks/rules-of-hooks */
+  return _has.HAS_KEYBOARD_FOCUS ? (0, _uiApi.useMemo)(() => [evt => {
     (0, _uiApi.setRefValue)(_refRecentFocusedElement, evt.target);
-  }, () => (0, _uiApi.focusRefElement)(_refRecentFocusedElement)], []);
+  }, () => (0, _uiApi.focusRefElement)(_refRecentFocusedElement)], []) : DF_TUPLE;
+  /*eslint-enable react-hooks/rules-of-hooks */
 };
 var _default = exports.default = useRecentFocusedElement;
 //# sourceMappingURL=useRecentFocusedElement.js.map
--- src/components/hooks/useRecentFocusedElement.js
+++ src/components/hooks/useRecentFocusedElement.js
@@ -1,18 +1,25 @@
 import {
+  FN_NOOP,
   useRef,
   useMemo,
   setRefValue,
   focusRefElement
 } from '../uiApi';
 
+import { HAS_KEYBOARD_FOCUS } from '../has';
+
+const DF_TUPLE = [void 0, FN_NOOP];
+
 const useRecentFocusedElement = () => {
   const _refRecentFocusedElement = useRef();
-  return useMemo(() => [
+  /*eslint-disable react-hooks/rules-of-hooks */
+  return HAS_KEYBOARD_FOCUS ? useMemo(() => [
     (evt) => {
       setRefValue(_refRecentFocusedElement, evt.target)
     },
     () => focusRefElement(_refRecentFocusedElement)
-  ], []);
+  ], []) : DF_TUPLE;
+  /*eslint-enable react-hooks/rules-of-hooks */
 };
 
 export default useRecentFocusedElement
