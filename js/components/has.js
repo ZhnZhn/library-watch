a3d7dfaccb423b7e42cb1ce390378c915b8cff7f
Yevhenii Molodyi <eugmolody@gmail.com>
2023-12-25 16:06:00 +0200

    Add HAS_KEYBOARD_FOCUS named export to components has namespace

--- js/components/has.js
+++ js/components/has.js
@@ -1,11 +1,9 @@
 "use strict";
 
 exports.__esModule = true;
-exports.isWideWidth = exports.HAS_TOUCH_EVENTS = void 0;
-var HAS_TOUCH_EVENTS = document && 'ontouchstart' in document.documentElement;
-exports.HAS_TOUCH_EVENTS = HAS_TOUCH_EVENTS;
-var isWideWidth = function isWideWidth() {
-  return window.innerWidth ? window.innerWidth > 700 : true;
-};
+exports.isWideWidth = exports.HAS_TOUCH_EVENTS = exports.HAS_KEYBOARD_FOCUS = void 0;
+const HAS_TOUCH_EVENTS = exports.HAS_TOUCH_EVENTS = document && 'ontouchstart' in document.documentElement;
+const HAS_KEYBOARD_FOCUS = exports.HAS_KEYBOARD_FOCUS = !HAS_TOUCH_EVENTS;
+const isWideWidth = () => window.innerWidth ? window.innerWidth > 700 : true;
 exports.isWideWidth = isWideWidth;
 //# sourceMappingURL=has.js.map
--- src/components/has.js
+++ src/components/has.js
@@ -1,5 +1,6 @@
 export const HAS_TOUCH_EVENTS = document
   && 'ontouchstart' in document.documentElement
+export const HAS_KEYBOARD_FOCUS = !HAS_TOUCH_EVENTS  
 
 export const isWideWidth = () => window.innerWidth
   ? window.innerWidth > 700
