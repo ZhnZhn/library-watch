648f6a2d8df73c7b9d8792b7e0814a072db40c20
Yevhenii Molodyi <eugmolody@gmail.com>
2024-02-13 11:13:51 +0200

    Add items stack config file

---
+++ js/components/items/stack/config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.__esModule = true;
+exports.PN_VIEW_COUNT = exports.PN_SCORE = exports.PN_REPUTATION = exports.PN_BOUNTY_AMOUNT = exports.PN_ANSWER_COUNT = void 0;
+const PN_BOUNTY_AMOUNT = exports.PN_BOUNTY_AMOUNT = "bounty_amount";
+const PN_ANSWER_COUNT = exports.PN_ANSWER_COUNT = "answer_count";
+const PN_SCORE = exports.PN_SCORE = "score";
+const PN_VIEW_COUNT = exports.PN_VIEW_COUNT = "view_count";
+const PN_REPUTATION = exports.PN_REPUTATION = "reputation";
+//# sourceMappingURL=config.js.map
---
+++ src/components/items/stack/config.js
@@ -0,0 +1,6 @@
+
+export const PN_BOUNTY_AMOUNT = "bounty_amount"
+export const PN_ANSWER_COUNT = "answer_count"
+export const PN_SCORE = "score"
+export const PN_VIEW_COUNT = "view_count"
+export const PN_REPUTATION = "reputation"
