042be41f63a6cce809a5fa1a9f0ec0d2964f09ce
Yevhenii Molodyi <eugmolody@gmail.com>
2024-02-16 13:08:41 +0200

    Add function memoItems named export to memoFn

--- js/components/hoc/memoFn.js
+++ js/components/hoc/memoFn.js
@@ -1,17 +1,19 @@
 "use strict";
 
 exports.__esModule = true;
-exports.memoIsShow = exports.memoEqual = void 0;
+exports.memoItems = exports.memoIsShow = exports.memoEqual = void 0;
 var _uiApi = require("../uiApi");
 const IS_NOT_SHOULD_RERENDER_DF = () => true;
-const memoEqual = function (Element, isNotShouldRerender) {
+const memoEqual = function (Comp, isNotShouldRerender) {
   if (isNotShouldRerender === void 0) {
     isNotShouldRerender = IS_NOT_SHOULD_RERENDER_DF;
   }
-  return (0, _uiApi.memo)(Element, isNotShouldRerender);
+  return (0, _uiApi.memo)(Comp, isNotShouldRerender);
 };
 exports.memoEqual = memoEqual;
+const _fMemo = isNotShouldRerender => Comp => (0, _uiApi.memo)(Comp, isNotShouldRerender);
 const _isNotShouldRerenderIsShow = (prevProps, nextProps) => prevProps.isShow === nextProps.isShow;
-const memoIsShow = Comp => (0, _uiApi.memo)(Comp, _isNotShouldRerenderIsShow);
-exports.memoIsShow = memoIsShow;
+const memoIsShow = exports.memoIsShow = _fMemo(_isNotShouldRerenderIsShow);
+const _isNotShouldRerenderItems = (prevProps, nextProps) => prevProps.items === nextProps.items;
+const memoItems = exports.memoItems = _fMemo(_isNotShouldRerenderItems);
 //# sourceMappingURL=memoFn.js.map
--- src/components/hoc/memoFn.js
+++ src/components/hoc/memoFn.js
@@ -2,14 +2,22 @@ import { memo } from '../uiApi';
 
 const IS_NOT_SHOULD_RERENDER_DF = () => true;
 export const memoEqual = (
-  Element,
+  Comp,
   isNotShouldRerender=IS_NOT_SHOULD_RERENDER_DF
-) => memo(Element, isNotShouldRerender)
+) => memo(Comp, isNotShouldRerender)
+
+const _fMemo = (
+  isNotShouldRerender
+) => Comp => memo(Comp, isNotShouldRerender);
 
 const _isNotShouldRerenderIsShow = (
   prevProps,
   nextProps
-) => prevProps.isShow === nextProps.isShow
-export const memoIsShow = (
-  Comp
-) => memo(Comp, _isNotShouldRerenderIsShow)
+) => prevProps.isShow === nextProps.isShow;
+export const memoIsShow = _fMemo(_isNotShouldRerenderIsShow)
+
+const _isNotShouldRerenderItems = (
+  prevProps,
+  nextProps
+) => prevProps.items === nextProps.items;
+export const memoItems = _fMemo(_isNotShouldRerenderItems)
