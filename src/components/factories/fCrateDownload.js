3e49066a8f0acb752eb22605ddd8df339d8b4946
Yevhenii Molodyi <eugmolody@gmail.com>
2024-11-15 17:32:54 +0200

    Improve components factories fCrateDownload

--- src/components/factories/fCrateDownload.js
+++ src/components/factories/fCrateDownload.js
@@ -8,11 +8,74 @@ import CrateDownloads from '../items/crate/CrateDownloads';
 const _getDate = item => (item || {}).date;
 const _getDownloads = item => (item || {}).downloads;
 
+const _getDateValue = item => [
+  _getDate(item),
+  _getDownloads(item)
+]
+
 const _crLabel = (date) => {
   const _dateTokens = date.split("-");
   return _dateTokens.length === 3
     ? `${_dateTokens[1]}-${_dateTokens[2]}`
     : date;
+};
+
+const _crHm = (extraDownloads) => {
+  const _hm = {};
+  for (const item of extraDownloads) {
+    const [
+      date,
+      value
+    ] = _getDateValue(item);
+    if (isNumber(value) && isStr(date)) {
+      _hm[date] = value
+    }
+  }
+  return _hm;
+}
+
+const _sumValuesTo = (
+  hm,
+  downloads
+) => {
+  for (const item of downloads) {
+    const [
+      date,
+      value
+    ] = _getDateValue(item);
+    if (isNumber(value) && isStr(date)) {
+      if (isNumber(hm[date])) {
+        hm[date] += value
+      } else {
+        hm[date] = value
+      }
+    }
+  }
+};
+
+const _crArrDateValue = (
+  hm
+) => Object
+.keys(hm)
+.map(key => [key, hm[key]])
+.sort((t1, t2) => t1 > t2 ? 1 : -1);
+
+const _crLabelsDataSum = (
+  arrDateValue
+) => {
+  const labels = []
+  , data = [];
+  let sumDownloads = 0;
+  for (const tuple of arrDateValue){
+    labels.push(_crLabel(tuple[0]))
+    data.push(tuple[1])
+    sumDownloads += tuple[1]
+  }
+  return [
+    labels,
+    data,
+    sumDownloads
+  ];
 };
 
 const fCrateDownload = (
@@ -30,29 +93,31 @@ const fCrateDownload = (
     browserType,
     key
   } = option
-  , labels = []
-  , data = []
-  , _downloads = json.meta.extra_downloads
-  , _downloadsLength = _downloads.length  
-  let sumDownloads = 0;
-  for (const item of _downloads) {
-    const _value = _getDownloads(item)
-    , _strDate = _getDate(item);
-    if (isNumber(_value) && isStr(_strDate)) {
-      labels.push(_crLabel(_strDate))
-      data.push(_value)
-      sumDownloads += _value
-    }
-  }
+  , {
+    version_downloads,
+    meta
+  } = json
+  , _extraDownloads = meta.extra_downloads
+  , _extraDownloadsLength = _extraDownloads.length;
+
+  const _hm = _crHm(_extraDownloads);
+  _sumValuesTo(_hm, version_downloads)
+  const arrDateValue = _crArrDateValue(_hm)
+  , [
+    labels,
+    data,
+    sumDownloads
+  ] = _crLabelsDataSum(arrDateValue);
+
   return createElement(CrateDownloads, {
     key,
     caption: repo,
     packageName: repo,
-    fromDate: _downloadsLength !== 0
-      ? _getDate(_downloads[0])
+    fromDate: _extraDownloadsLength !== 0
+      ? _getDate(_extraDownloads[0])
       : "",
-    toDate: _downloadsLength > 0
-      ? _getDate(_downloads[_downloadsLength-1])
+    toDate: _extraDownloadsLength > 0
+      ? _getDate(_extraDownloads[_extraDownloadsLength-1])
       : "",
     sumDownloads,
     labels,
