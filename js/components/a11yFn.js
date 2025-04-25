7f04e5d9978dac8fba226793b2716db9d7505aff
Yevhenii Molodyi <eugmolody@gmail.com>
2025-04-25 11:54:25 +0300

    Add components a11yFn

---
+++ js/components/a11yFn.js
@@ -0,0 +1,11 @@
+"use strict";
+
+exports.__esModule = true;
+exports.crA11yComboboxProps = void 0;
+const crA11yComboboxProps = labelId => ({
+  role: "combobox",
+  "aria-autocomplete": "list",
+  "aria-labelledby": labelId
+});
+exports.crA11yComboboxProps = crA11yComboboxProps;
+//# sourceMappingURL=a11yFn.js.map
---
+++ src/components/a11yFn.js
@@ -0,0 +1,7 @@
+export const crA11yComboboxProps = (
+  labelId
+) => ({
+  role: "combobox",
+  "aria-autocomplete": "list",
+  "aria-labelledby": labelId
+})
