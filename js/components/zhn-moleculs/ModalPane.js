c941e03fa13b8da3496eafa96b8ffa87ca7900f4
Yevhenii Molodyi <eugmolody@gmail.com>
2025-08-18 12:35:20 +0300

    Add useKeyEscape to function component ModalPane

--- js/components/zhn-moleculs/ModalPane.js
+++ js/components/zhn-moleculs/ModalPane.js
@@ -4,6 +4,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports.default = void 0;
 var _useClickOutside = _interopRequireDefault(require("../hooks/useClickOutside"));
+var _useKeyEscape = _interopRequireDefault(require("../hooks/useKeyEscape"));
 var _jsxRuntime = require("react/jsx-runtime");
 const S_DIV = {
     backgroundColor: '#4d4d4d'
@@ -12,15 +13,28 @@ const S_DIV = {
 const ModalPane = _ref2 => {
   let {
     isShow,
+    className,
+    style,
     onClose = FN_NOOP,
-    children
+    children,
+    ...restProps
   } = _ref2;
-  const _ref = (0, _useClickOutside.default)(isShow, onClose);
+  const _ref = (0, _useClickOutside.default)(isShow, onClose),
+    _hKeyEscape = (0, _useKeyEscape.default)(onClose);
+  /*eslint-disable jsx-a11y/no-static-element-interactions*/
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    ...restProps,
     ref: _ref,
-    style: S_DIV,
+    className: className,
+    style: {
+      ...S_DIV,
+      ...style
+    },
+    hidden: !isShow,
+    onKeyDown: isShow ? _hKeyEscape : void 0,
     children: children
   });
+  /*eslint-enable jsx-a11y/no-static-element-interactions*/
 };
 var _default = exports.default = ModalPane;
 //# sourceMappingURL=ModalPane.js.map
--- src/components/zhn-moleculs/ModalPane.jsx
+++ src/components/zhn-moleculs/ModalPane.jsx
@@ -1,22 +1,33 @@
 import useClickOutside from '../hooks/useClickOutside';
+import useKeyEscape from '../hooks/useKeyEscape';
 
 const S_DIV = { backgroundColor: '#4d4d4d' }
 , FN_NOOP = () => {};
 
 const ModalPane = ({
   isShow,
+  className,
+  style,
   onClose=FN_NOOP,
-  children
+  children,
+  ...restProps
 }) => {
-  const _ref = useClickOutside(isShow, onClose);
+  const _ref = useClickOutside(isShow, onClose)
+  , _hKeyEscape = useKeyEscape(onClose);
+  /*eslint-disable jsx-a11y/no-static-element-interactions*/
   return (
     <div
+       {...restProps}
        ref={_ref}
-       style={S_DIV}
+       className={className}
+       style={{...S_DIV, ...style}}
+       hidden={!isShow}
+       onKeyDown={isShow ? _hKeyEscape : void 0}
     >
       {children}
     </div>
   );
+  /*eslint-enable jsx-a11y/no-static-element-interactions*/
 };
 
 export default ModalPane
